1a37f1efb008f96d325a45077d3869cb869d4ffe
3li-3bdullah <PROGRMMER7@gmail.com>
2022-02-06 23:11:04 +0200

    Initialized the firebase

--- sudaphone_sd/lib/main.dart
+++ sudaphone_sd/lib/main.dart
@@ -3,24 +3,32 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone_sd/my_binding/my_binding.dart';
 import 'package:sudaphone_sd/view/mydrawer.dart';
+// import 'package:sudaphone_sd/view/screen.dart';
 
-main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "SudaPhone SD",
-      theme: ThemeData(primaryColor: Colors.white),
-      initialBinding: MyBinding(),
-      home: const MyDrawer(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: "SudaPhone SD",
+        theme: ThemeData(primaryColor: Colors.white),
+        initialBinding: MyBinding(),
+        home: FutureBuilder(
+            future: _fbApp,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text("You have an error! ${snapshot.error.toString()}");
+              } else if (snapshot.hasData) {
+                return const MyDrawer();
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }));
   }
 }
