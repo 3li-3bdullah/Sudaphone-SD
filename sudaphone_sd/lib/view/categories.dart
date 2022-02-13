6f8791ea42fb9c503136e8306e3e58abbd5eed32
3li-3bdullah <PROGRMMER7@gmail.com>
2022-02-13 17:23:56 +0200

    I've changed the color and elevation

--- sudaphone_sd/lib/view/categories.dart
+++ sudaphone_sd/lib/view/categories.dart
@@ -21,100 +21,101 @@ class Categories extends StatelessWidget {
 
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Directionality(
-            textDirection: TextDirection.rtl,
-            child: Scaffold(
-              appBar: AppBar(
-                title: const Text(
-                    "الأقسام"
-                    ),
-                centerTitle: true,
-              ),
-
-              /// drawer: MyDrawer(),
-              body: Container(
-                height: double.infinity,
-                width: double.infinity,
-                padding: const EdgeInsets.all(5),
-                child: GridView(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  children: [
-                    CategoriesCard(
-                      imageCat: "assets/images/category/samsung.png",
-                      text: "Samsung",
-                      onTap: () {
-                        Get.to(Samsung(),
-                            transition: Transition.zoom,
-                            curve: Curves.bounceInOut);
-                      },
-                    ),
-                    CategoriesCard(
-                      imageCat: "assets/images/category/huawei.png",
-                      text: "Huawei",
-                      onTap: () {
-                        Get.to(Huawei());
-                      },
-                    ),
-                    CategoriesCard(
-                      imageCat: "assets/images/category/apple.jpg",
-                      text: "Iphone",
-                      onTap: () {
-                        Get.to(Iphone());
-                      },
-                    ),
-                    CategoriesCard(
-                      imageCat: "assets/images/category/oppo.png",
-                      text: "Oppo",
-                      onTap: () {
-                        Get.to(Oppo());
-                      },
-                    ),
-                    CategoriesCard(
-                      imageCat: "assets/images/category/xiaomi.png",
-                      text: "Xiaomi",
-                      onTap: () {
-                        Get.to(Xiaomi());
-                      },
-                    ),
-                    CategoriesCard(
-                      imageCat: "assets/images/category/lenovo.png",
-                      text: "Lenovo",
-                      onTap: () {
-                        Get.to(Lenovo());
-                      },
-                    ),
-                    CategoriesCard(
-                      imageCat: "assets/images/category/realme.png",
-                      text: "Realme",
-                      onTap: () {
-                        Get.to(Realme());
-                      },
-                    ),
-                    CategoriesCard(
-                      imageCat: "assets/images/category/tecno.png",
-                      text: "Tecno",
-                      onTap: () {
-                        Get.to(Tecno());
-                      },
-                    ),
-                    CategoriesCard(
-                      imageCat: "assets/images/category/vivo.jpg",
-                      text: "Vivo",
-                      onTap: () {
-                        Get.to(Vivo());
-                      },
-                    ),
-                    CategoriesCard(
-                      imageCat: "assets/images/category/nokia.png",
-                      text: "Nokia",
-                      onTap: () {
-                        Get.to(Nokia());
-                      },
-                    ),
-                  ],
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+                "Categories"
+                ,style: TextStyle(color:Colors.black),
                 ),
-              ),
-            )));
+                backgroundColor: Colors.white,
+            centerTitle: true,
+            elevation: 0,
+          ),
+
+          /// drawer: MyDrawer(),
+          body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            padding: const EdgeInsets.all(5),
+            child: GridView(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              children: [
+                CategoriesCard(
+                  imageCat: "assets/images/category/samsung.png",
+                  text: "Samsung",
+                  onTap: () {
+                    Get.to(Samsung(),
+                        transition: Transition.zoom,
+                        curve: Curves.bounceInOut);
+                  },
+                ),
+                CategoriesCard(
+                  imageCat: "assets/images/category/huawei.png",
+                  text: "Huawei",
+                  onTap: () {
+                    Get.to(Huawei());
+                  },
+                ),
+                CategoriesCard(
+                  imageCat: "assets/images/category/apple.jpg",
+                  text: "Iphone",
+                  onTap: () {
+                    Get.to(Iphone());
+                  },
+                ),
+                CategoriesCard(
+                  imageCat: "assets/images/category/oppo.png",
+                  text: "Oppo",
+                  onTap: () {
+                    Get.to(Oppo());
+                  },
+                ),
+                CategoriesCard(
+                  imageCat: "assets/images/category/xiaomi.png",
+                  text: "Xiaomi",
+                  onTap: () {
+                    Get.to(Xiaomi());
+                  },
+                ),
+                CategoriesCard(
+                  imageCat: "assets/images/category/lenovo.png",
+                  text: "Lenovo",
+                  onTap: () {
+                    Get.to(Lenovo());
+                  },
+                ),
+                CategoriesCard(
+                  imageCat: "assets/images/category/realme.png",
+                  text: "Realme",
+                  onTap: () {
+                    Get.to(Realme());
+                  },
+                ),
+                CategoriesCard(
+                  imageCat: "assets/images/category/tecno.png",
+                  text: "Tecno",
+                  onTap: () {
+                    Get.to(Tecno());
+                  },
+                ),
+                CategoriesCard(
+                  imageCat: "assets/images/category/vivo.jpg",
+                  text: "Vivo",
+                  onTap: () {
+                    Get.to(Vivo());
+                  },
+                ),
+                CategoriesCard(
+                  imageCat: "assets/images/category/nokia.png",
+                  text: "Nokia",
+                  onTap: () {
+                    Get.to(Nokia());
+                  },
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
