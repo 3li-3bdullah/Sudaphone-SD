0a21024e2398a1f5c5fc1427dff5d10400f8cff0
3li-3bdullah <PROGRMMER7@gmail.com>
2022-02-13 17:28:09 +0200

    Added alot of edit and till now still under testing

--- sudaphone_sd/lib/view/posts_widgets/card_view.dart
+++ sudaphone_sd/lib/view/posts_widgets/card_view.dart
@@ -1,107 +1,138 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sudaphone_sd/model/posts_model.dart';
-import 'package:sudaphone_sd/view/comments.dart';
+import 'package:sudaphone_sd/view/posts_pages/comments.dart';
+import 'package:sudaphone_sd/view/download_images.dart';
 import 'package:sudaphone_sd/view/widgets/custom_text.dart';
 import 'package:sudaphone_sd/view_model/posts_view_model.dart';
 
 // ignore: must_be_immutable
-class CardView extends StatelessWidget {
-  CardView({Key? key , required this.controller, required this.postsModel}) : super(key: key);
-  PostsModel? postsModel;
-  PostsViewModel? controller;
+class CardView extends GetWidget<PostsViewModel> {
+  CardView({Key? key, required this.data}) : super(key: key);
+  var data;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Card(
-            child: Column(children: [
-      ListTile(
-          leading: const CircleAvatar(
-              backgroundImage: AssetImage("assets/images/slider/ali.jpg")),
-          title: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 10),
-                child: const CustomText(
-                  text: "Ali Abdullah",
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  textAlign: TextAlign.right,
-                ),
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              leading: const CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/slider/ali1.jpg")),
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    child: const CustomText(
+                      text: "Ali Abdullah",
+                      color: Colors.black,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                  // CustomText(
+                  //   text: "${data.data()['dateTime'].toDate().toString()}",
+                  //   fontSize: 13,
+                  //   fontWeight: FontWeight.normal,
+                  //   color: Colors.grey,
+                  //   textAlign: TextAlign.right,
+                  // )
+                ],
               ),
-              CustomText(
-                text: "${postsModel!.postDate.toString()}",
-                fontSize: 15,
-                fontWeight: FontWeight.normal,
-                color: Colors.grey,
-                textAlign: TextAlign.right,
-              )
-            ],
-          ),
-          trailing:
-              IconButton(icon: const Icon(Icons.more_horiz), onPressed: () {}),
-          isThreeLine: true,
-          subtitle: Column(children: [
-            CustomText(
-              text: "${postsModel!.text}",
-              color: Colors.black,
-              fontSize: 17,
-              fontWeight: FontWeight.normal,
-              textAlign: TextAlign.center,
-            ),
-            Image.network("${postsModel!.imageUrl.toString()}"),
-          ])),
-      Divider(color: Colors.grey.withOpacity(0.2)),
-      Row(children: [
-        //After test make this widget custom..............
-        Expanded(
-          child: InkWell(
-            child: Container(
-              decoration: const BoxDecoration(
-                  border: Border(left: BorderSide(color: Colors.grey))),
-              child:
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                CustomText(
-                  text: controller!.like == false ? "0" : "1",
-                  color: Colors.grey.shade800,
-                  fontSize: 15,
-                  fontWeight: FontWeight.normal,
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(width: 4),
-                controller!.like == false
-                    ? Icon(Icons.favorite_border_outlined,
-                        color: Colors.pink.shade600)
-                    : Icon(Icons.favorite, color: Colors.pink.shade600)
-              ]),
-            ),
-            onTap: () {
-              controller!.isLiked();
-            },
-          ),
-        ),
-        Expanded(
-          child: InkWell(
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              CustomText(
-                text: "0",
-                color: Colors.grey.shade800,
-                fontSize: 15,
-                fontWeight: FontWeight.normal,
-                textAlign: TextAlign.center,
+              trailing: IconButton(
+                  icon: const Icon(Icons.more_horiz), onPressed: () {}),
+              isThreeLine: true,
+              subtitle: Column(
+                children: [
+                  CustomText(
+                    text: "${data.data()['text']}",
+                    color: Colors.black,
+                    fontSize: 17,
+                    fontWeight: FontWeight.normal,
+                    textAlign: TextAlign.center,
+                  ),
+                  InkWell(
+                    child: Image.network("${data.data()['imageUrl']}"),
+                    onTap: () {
+                      Get.to(
+                          () => DownloadImages(image: data.data()['imageUrl']),
+                          transition: Transition.zoom,
+                          curve: Curves.easeInExpo);
+                    },
+                  ),
+                ],
               ),
-              const SizedBox(width: 4),
-              const Icon(Icons.insert_comment_outlined, color: Colors.green),
-            ]),
-            onTap: () {
-              Get.to(() => const Comments(), transition: Transition.fadeIn);
-            },
-          ),
+            ),
+            Divider(color: Colors.grey.withOpacity(0.2)),
+            Row(
+              children: [
+                Expanded(
+                  child: GetBuilder<PostsViewModel>(
+                    builder: (controller) => Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            controller.isLiked();
+                          },
+                          child: controller.like == true
+                              ? const Icon(Icons.favorite, color: Colors.pink)
+                              : const Icon(Icons.favorite_border_outlined,
+                                  color: Colors.grey),
+                        ),
+                        controller.like == true
+                            ? const Text(
+                                "1",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.pink),
+                              )
+                            : Text(
+                                "0",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.grey.shade800,
+                                ),
+                              ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Get.to(() => const Comments());
+                        },
+                        child: const Icon(
+                          Icons.comment_outlined,
+                          color: Colors.green,
+                        ),
+                      ),
+                      CustomText(
+                        text: "0",
+                        color: Colors.grey.shade800,
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 5),
+            ),
+          ],
         ),
-      ]),
-      const Padding(padding: EdgeInsets.only(top: 5)),
-    ])));
+      ),
+    );
   }
 }
