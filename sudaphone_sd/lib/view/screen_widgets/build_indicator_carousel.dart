452b7e01e669384bd119d4dd500c9b79720242ad
3li-3bdullah <PROGRMMER7@gmail.com>
2022-02-18 17:28:20 +0200

    Removed the widgets to alone file :)

---
+++ sudaphone_sd/lib/view/screen_widgets/build_indicator_carousel.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:sudaphone_sd/view_model/screen_view_model.dart';
+
+class BuildIndicatorCarousel extends GetWidget<ScreenViewModel> {
+  const BuildIndicatorCarousel({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SmoothPageIndicator(
+        controller: controller.controllerCarousel,
+        count: controller.imagesCarousel.length,
+        effect: CustomizableEffect(
+          activeDotDecoration: DotDecoration(
+            width: 32,
+            height: 12,
+            color: Colors.white,
+            rotationAngle: 180,
+            verticalOffset: -10,
+            borderRadius: BorderRadius.circular(24),
+            dotBorder: const DotBorder(
+              padding: 2,
+              width: 2,
+              color: Colors.black,
+            ),
+          ),
+          dotDecoration: const DotDecoration(
+            width: 24,
+            height: 12,
+            color: Colors.grey,
+            dotBorder: DotBorder(
+              padding: 2,
+              width: 2,
+              color: Colors.grey,
+            ),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(2),
+                topRight: Radius.circular(16),
+                bottomLeft: Radius.circular(16),
+                bottomRight: Radius.circular(2)),
+            //borderRadius: BorderRadius.circular(16),
+            verticalOffset: 0,
+          ),
+          spacing: 6.0,
+          // activeColorOverride: (i) => colors[i],
+          inActiveColorOverride: (i) => controller.colors[i],
+        ),
+      );
+  }
+}
