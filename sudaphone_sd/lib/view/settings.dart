7ee3b3148f74b6fbdc798226fccd496cbec7df81
3li-3bdullah <PROGRMMER7@gmail.com>
2022-03-05 21:32:31 +0200

    Still under testing

--- sudaphone_sd/lib/view/settings.dart
+++ sudaphone_sd/lib/view/settings.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+// ignore: must_be_immutable
 class Settings extends StatelessWidget {
-  const Settings({Key? key}) : super(key: key);
+  Settings({Key? key}) : super(key: key);
+
+  //  Settings({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
           title: const Text(
-               'Settings',style:TextStyle(color: Colors.black)),
+            'Settings',
+            style: TextStyle(color: Colors.black),
+          ),
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Get.back();
+            },
+          ),
           centerTitle: true,
           elevation: 0,
-          backgroundColor:Colors.white
-        ),
-        body: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 5,vertical: 20),
-            child: ListTile(
+          backgroundColor: Colors.white),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+        child: Column(
+          children: [
+            ListTile(
               title: const Text("The Mode"),
               trailing: const Icon(Icons.wb_sunny),
               onTap: () {
@@ -39,6 +54,44 @@ class Settings extends StatelessWidget {
                   ],
                 ));
               },
-            )));
+            ),
+            // ElevatedButton(onPressed: () => getImage() , child: const Text("Check an image")),
+            // const SizedBox(height: 20,),
+            // ElevatedButton(onPressed: () => uploadImage() , child: const Text("Upload Image")),
+            // Container(
+            //   height: 300,
+            //   width: 300,
+            //   child: filePicked == null? const SizedBox() : Image.asset(pickedImage!.path),
+            // )
+          ],
+        ),
+        
+      ),
+    );
   }
+//   String? imageName;
+//   File? filePicked;
+//    XFile? pickedImage;
+//   String dateTime =
+//       DateFormat('M/d/y - kk:mm').format(DateTime.now());
+//       //===================
+//  Future getImage() async {
+//     ImagePicker _picker = ImagePicker();
+//      pickedImage = await _picker.pickImage(source: ImageSource.gallery);
+//     if(pickedImage == null){
+//       return null;
+//     }
+//      imageName = basename(pickedImage!.path); 
+//      filePicked = File(pickedImage!.path);
+//   }
+//   Future uploadImage() async {
+//   var reference = FirebaseStorage.instance.ref("images").child(imageName!);
+//     reference.putFile(filePicked!);
+//     String imageUrl = await reference.getDownloadURL();
+//     FirebaseFirestore.instance.collection("users").add({
+//       "imageUrl" : imageUrl,
+//       "dateTime" : dateTime.toString()
+//     });
+//     Get.off(() => const Posts() , transition: Transition.zoom);
+//   }
 }
