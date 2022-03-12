362b67b8cbc475588d066a706218cbe56269e653
3li-3bdullah <PROGRMMER7@gmail.com>
2022-03-12 22:48:47 +0200

    Added condition to see if the user has login or not

--- sudaphone_sd/lib/main.dart
+++ sudaphone_sd/lib/main.dart
@@ -1,45 +1,59 @@
-import 'dart:async';
+import 'dart:ui';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone_sd/my_binding/my_binding.dart';
+import 'package:sudaphone_sd/view/login.dart';
 import 'package:sudaphone_sd/view/mydrawer.dart';
+import 'package:sudaphone_sd/view/posts.dart';
+import 'package:sudaphone_sd/view/screen.dart';
+import 'package:sudaphone_sd/view_model/main_view_model.dart';
 
- main() async {
-   WidgetsFlutterBinding.ensureInitialized();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(SudaphoneSD());
+  runApp(const SudaphoneSD());
 }
 
-// ignore: use_key_in_widget_constructors, must_be_immutable
 class SudaphoneSD extends StatelessWidget {
-   Future<FirebaseApp>? fbApp = Firebase.initializeApp();
+  const SudaphoneSD({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    Get.testMode = true;
     return GetMaterialApp(
-      // key: Get.key,
       debugShowCheckedModeBanner: false,
       title: "SudaPhone SD",
       theme: ThemeData(primaryColor: Colors.white),
       initialBinding: MyBinding(),
-      home: FutureBuilder(
-        future: fbApp!,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Text("You have an error! : ${snapshot.error.toString()}");
-          } else if (snapshot.hasData) {
-            return  const MyDrawer();
-          } else {
-            return Container(
-              color: Colors.white,
-              child: const Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          }
-        },
-      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends GetWidget<MainViewModel> {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: controller.fbApp,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text("You have an error! : ${snapshot.error.toString()}");
+        } else if (snapshot.hasData) {
+          return const LogIn();
+          // controller.userUid == null ?  const LogIn() : const MyDrawer();
+        } else {
+          return Container(
+            color: Colors.white,
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
     );
   }
 }
