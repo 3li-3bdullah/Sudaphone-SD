99255220a35516fbf36b9cd679f12b48331f4ae3
3li-3bdullah <PROGRMMER7@gmail.com>
2022-03-17 23:15:10 +0200

    I've added function to see if there user or not

--- sudaphone_sd/lib/view_model/main_view_model.dart
+++ sudaphone_sd/lib/view_model/main_view_model.dart
@@ -3,9 +3,24 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class MainViewModel extends GetxController{
-String uid = FirebaseAuth.instance.currentUser!.uid;
 Future<FirebaseApp> fbApp = Firebase.initializeApp();
+FirebaseAuth auth = FirebaseAuth.instance;
+RxBool isLogin = false.obs;
 
 
+///Declaring Methods
+  @override
+  void onInit() {
+    super.onInit();
+    refreshFirebaseUser();
+  }
+Future refreshFirebaseUser() async {
+  if (auth.currentUser != null) {
+    isLogin.value = true;
+    final user = auth.currentUser;
+    final idTokenResult = await user!.getIdTokenResult(true);
+    return idTokenResult;
+  }
+}
 
 }
