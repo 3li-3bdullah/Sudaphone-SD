0620bb95754fb898031b2750edee2f94560ef478
3li-3bdullah <PROGRMMER7@gmail.com>
2022-03-19 19:11:49 +0200

    I've added some injections

--- sudaphone_sd/lib/my_binding/my_binding.dart
+++ sudaphone_sd/lib/my_binding/my_binding.dart
@@ -1,4 +1,8 @@
 import 'package:get/get.dart';
+import 'package:sudaphone_sd/view_model/download_images_view_model.dart';
+import 'package:sudaphone_sd/view_model/login_view_model.dart';
+import 'package:sudaphone_sd/view_model/main_view_model.dart';
+import 'package:sudaphone_sd/view_model/mydrawer_view_model.dart';
 import 'package:sudaphone_sd/view_model/posts_view_model.dart';
 import 'package:sudaphone_sd/view_model/screen_view_model.dart';
 
@@ -6,6 +10,10 @@ class MyBinding implements Bindings {
   @override
   void dependencies() {
     Get.put(ScreenViewModel());
-    Get.lazyPut(() => PostsViewModel(),fenix:true);
+    Get.put(MyDrawerViewModel());
+    Get.put(LoginViewModel());
+    Get.lazyPut(() => PostsViewModel(), fenix: true);
+    Get.put<MainViewModel>(MainViewModel());
+    Get.lazyPut(() => DownloadImagesViewModel(), fenix: true);
   }
 }
