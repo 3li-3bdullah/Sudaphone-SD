f8098ac4f86a391ab80ec7dbbe6fa0a1e51d89a2
3li-3bdullah <PROGRMMER7@gmail.com>
2022-03-20 23:43:19 +0200

    I've changed  the icons with images

--- sudaphone_sd/lib/view/bottom_navigation.dart
+++ sudaphone_sd/lib/view/bottom_navigation.dart
@@ -11,39 +11,60 @@ import 'package:sudaphone_sd/view_model/screen_view_model.dart';
 class BottomNavigation extends GetWidget<ScreenViewModel> {
   BottomNavigation({Key? key}) : super(key: key);
   final List<Widget> screens = [
-    Screen(),
+    const Screen(),
     const Categories(),
     const Posts(),
-    const Settings(),
+     Settings(),
   ];
   @override
   Widget build(BuildContext context) {
     return GetX<ScreenViewModel>(
       builder: (controller) => Scaffold(
         bottomNavigationBar: BottomNavyBar(
-            selectedIndex: controller.activeIndex!.value,
-            showElevation: true,
-            onItemSelected: (index) {
-              controller.activeIndex!.value = index;
-            },
-            items: [
-              BottomNavyBarItem(
-                  icon: const Icon(Icons.apps),
-                  title: const Text("Home"),
-                  activeColor: Colors.red),
-              BottomNavyBarItem(
-                  icon: const Icon(Icons.category),
-                  title: const Text("Categories"),
-                  activeColor: Colors.purpleAccent),
-              BottomNavyBarItem(
-                  icon: const Icon(Icons.post_add),
-                  title: const Text("Posts"),
-                  activeColor: Colors.pink),
-              BottomNavyBarItem(
-                  icon: const Icon(Icons.settings),
-                  title: const Text("Settings"),
-                  activeColor: Colors.blue),
-            ]),
+          selectedIndex: controller.activeIndex!.value,
+          showElevation: true,
+          onItemSelected: (index) {
+            controller.activeIndex!.value = index;
+          },
+          items: [
+            BottomNavyBarItem(
+                icon: const Image(
+                  image: AssetImage("assets/images/icons/home.png"),
+                  color: Colors.red,
+                  height: 25,
+                  width: 25,
+                ),
+                title: const Text("Home"),
+                activeColor: Colors.red),
+            BottomNavyBarItem(
+                icon: const Image(
+                  image: AssetImage("assets/images/icons/category.png"),
+                  color: Colors.purpleAccent,
+                  height: 25,
+                  width: 25,
+                ),
+                title: const Text("Categories"),
+                activeColor: Colors.purpleAccent),
+            BottomNavyBarItem(
+                icon: const Image(
+                  image: AssetImage("assets/images/icons/add.png"),
+                  color: Colors.pink,
+                  height: 25,
+                  width: 25,
+                ),
+                title: const Text("Posts"),
+                activeColor: Colors.pink),
+            BottomNavyBarItem(
+                icon: const Image(
+                  image: AssetImage("assets/images/icons/settings.png"),
+                  color: Colors.blue,
+                  height: 25,
+                  width: 25,
+                ),
+                title: const Text("Settings"),
+                activeColor: Colors.blue),
+          ],
+        ),
         body: screens[controller.activeIndex!.value],
       ),
     );
