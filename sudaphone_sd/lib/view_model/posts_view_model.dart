4c8b0740dbc7eb16309e011bf603f1479409846e
3li-3bdullah <PROGRMMER7@gmail.com>
2022-03-21 23:05:25 +0200

    Added a lot of enhance and changed the Data Architectures

--- sudaphone_sd/lib/view_model/posts_view_model.dart
+++ sudaphone_sd/lib/view_model/posts_view_model.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -17,14 +18,17 @@ class PostsViewModel extends GetxController {
   CollectionReference? getData = FirebaseFirestore.instance.collection("users");
   late CollectionReference _collectionReference;
 
+  CollectionReference postsCollections = FirebaseFirestore.instance.collection("posts");
+
   String? _fileName;
-  // String? imageUrl;
-  bool like = false;
+  String? imageUrl;
+  RxBool isLiked = false.obs;
   File? _imageFile;
   XFile? pickedImage;
   TextEditingController? textController;
   RxBool isThereImageUrl = false.obs;
-  TextEditingController? imageController ;
+  RxBool? isPicked = false.obs;
+  FirebaseAuth? _auth = FirebaseAuth.instance;
 
   /// All Methods
   String? formattedDate;
@@ -34,56 +38,137 @@ class PostsViewModel extends GetxController {
     super.onInit();
     _collectionReference = addNewData!.collection("users");
     textController = TextEditingController();
-    imageController = TextEditingController();
   }
 
   // Upload Image From Camera or Gallery ...
-  Future<void> uploadImage(String source) async {
+  Future<bool?> uploadImage(String? source) async {
     final picker = ImagePicker();
     pickedImage = await picker.pickImage(
         source: source == "camera" ? ImageSource.camera : ImageSource.gallery);
     int rand = Random().nextInt(1000000);
     _fileName = rand.toString() + pickedImage!.name;
     _imageFile = File(pickedImage!.path);
+    if (source != null) {
+      return isPicked!.value = true;
+    } else {
+      return isPicked!.value = false;
+    }
     // update();
   }
 
   //Add Post
-  Future addPost(String? text,String? imageUrl , GlobalKey<FormState>? textFieldkey) async {
+  Future addPost(String? text, GlobalKey<FormState>? textFieldkey) async {
     final formState = textFieldkey!.currentState;
     if (formState!.validate()) {
       formState.save();
+      final formattedDate = DateFormat('M/d/y - kk:mm').format(DateTime.now());
       try {
-        if (_fileName != null) {
-         
+        if (isPicked!.value) {
           TaskSnapshot uploadToStorage = await FirebaseStorage.instance
               .ref("images")
               .child(_fileName!)
               .putFile(_imageFile!);
           imageUrl = await uploadToStorage.ref.getDownloadURL();
           isHasAnImageUrl(imageUrl);
+          isPicked!.value = false;
+          postsCollections
+              .doc("${_auth!.currentUser!.uid}")
+              .collection("userPosts")
+              .add({
+                "text": text!,
+                "imageUrl": imageUrl.toString(),
+                "dateTime": formattedDate.toString(),
+                "ownerUid": _auth!.currentUser!.uid.toString(),
+                "isThereImageUrl": isThereImageUrl.value,
+                "likesCount": 0,
+                "isHasLiked": false,
+              }).whenComplete(
+                () => CustomSnakbar.showSnakBar(
+                    backgroundColor: const Color.fromARGB(255, 188, 204, 189),
+                    context: Get.context,
+                    message: "Uploaded Post Successfully",
+                    title: "Done"),
+              )
+              .then(
+                (value) => Get.off(() => const Posts()),
+              );
+          // _collectionReference
+          //     .add({
+          //       "text": text!,
+          //       "imageUrl": imageUrl.toString(),
+          //       "dateTime": formattedDate.toString(),
+          //       "isThereImageUrl": isThereImageUrl.value
+          //     })
+          //     .whenComplete(
+          //       () => CustomSnakbar.showSnakBar(
+          //           backgroundColor: const Color.fromARGB(255, 188, 204, 189),
+          //           context: Get.context,
+          //           message: "Uploaded Post Successfully",
+          //           title: "Done"),
+          //     )
+          //     .then(
+          //       (value) => Get.off(() => const Posts()),
+          //     );
+        } else {
+          // _collectionReference
+          //     .add({
+          //       "text": text!,
+          //       "imageUrl": "null",
+          //       "dateTime": formattedDate.toString(),
+          //       "isThereImageUrl": false
+          //     })
+              postsCollections
+              .doc("${_auth!.currentUser!.uid}")
+              .collection("userPosts")
+              .add({
+                "text": text!,
+                "imageUrl": "null",
+                "dateTime": formattedDate.toString(),
+                "ownerUid": _auth!.currentUser!.uid.toString(),
+                "isThereImageUrl": false,
+                "likesCount": 0,
+                "isHasLiked": false,
+              }).whenComplete(
+                () => CustomSnakbar.showSnakBar(
+                    backgroundColor: const Color.fromARGB(255, 188, 204, 189),
+                    context: Get.context,
+                    message: "Uploaded Post Successfully",
+                    title: "Done"),
+              )
+              .then(
+                (value) => Get.off(() => const Posts()),
+              );
         }
-      final formattedDate = DateFormat('M/d/y - kk:mm').format(DateTime.now());
-        _collectionReference.add({
-          "text": text!,
-          "imageUrl": imageUrl.toString(),
-          "dateTime": formattedDate.toString(),
-          "isThereImageUrl": isThereImageUrl.value
-        }).whenComplete(() =>
-          // return imageUrl = null;
-          Get.off(() => const Posts()),
-         
-        ).then(
-          (value) =>  CustomSnakbar.showSnakBar(
-              backgroundColor: const Color.fromARGB(255, 188, 204, 189),
-              context: Get.context,
-              message: "Uploaded Post Successfully",
-              title: "Done"),);
-        
       } catch (e) {
         return Get.snackbar("Error", "The error : ${e.toString()}");
       }
       clearEditingControllers();
+      resetValues(fileName: _fileName, imageFile: _imageFile, url: imageUrl);
+    }
+  }
+
+  // Handle Likes
+  handleLikes(likesData) async {
+    DocumentReference<Map<String, dynamic>> _likeData = FirebaseFirestore
+        .instance
+        .collection("posts")
+        .doc("${likesData.data()['ownerUid']}")
+        .collection("userPosts")
+        .doc(likesData);
+    // The option will show up if the owner user has liked the post before
+    // or not, and even if the user delete the app and reinstall again , so
+    // he'll see he has liked before or not.
+    bool _isHasLiked =
+        await _likeData.get().then((value) => value.data()!['isHasLiked']);
+    if (!isLiked.value && !_isHasLiked) {
+      int _addLike = likesData.data()['likesCount'] + 1;
+      await _likeData.update({'likesCount': _addLike.toInt(), 'isHasLiked': true});
+      isLiked.value = true;
+    } else {
+      int _removeLike = likesData.data()['likesCount'] - 1;
+      await _likeData
+          .update({'likesCount': _removeLike.toInt(), 'isHasLiked': false});
+      isLiked.value = false;
     }
   }
 
@@ -94,16 +179,11 @@ class PostsViewModel extends GetxController {
       return isThereImageUrl.value = false;
     }
   }
-  // whenPostAdded(){
-  //   imageUrl = "null";
-  //   isThereImageUrl.value = false;
-  // }
-
   // Added To Post Like Or Not ...
-  void isLiked() {
-    like = !like;
-    update();
-  }
+  // void isLiked() {
+  //   like = !like;
+  //   update();
+  // }
 
   String? isValid(String? valid) {
     if (valid!.isEmpty) {
@@ -111,17 +191,20 @@ class PostsViewModel extends GetxController {
     }
     return null;
   }
+
   void clearEditingControllers() {
     textController!.clear();
-    imageController!.clear();
-   
   }
-  
+
+  void resetValues({String? fileName, File? imageFile, String? url}) {
+    fileName = null;
+    imageFile = null;
+    url = null;
+  }
+
   @override
   void onClose() {
     super.onClose();
-    // imageUrl!.dispose();
     textController!.dispose();
-    imageController!.dispose();
   }
 }
