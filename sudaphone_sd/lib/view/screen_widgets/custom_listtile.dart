5954795b31c4d58cb548f6ac35308c470341ae61
3li-3bdullah <PROGRMMER7@gmail.com>
2022-04-12 00:11:18 +0200

    I've created new class and i wrote listTile widget and some widgets inside

---
+++ sudaphone_sd/lib/view/screen_widgets/custom_listtile.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CustomListTile extends StatelessWidget {
+  CustomListTile(
+      {required this.text, required this.image, required this.onTap, Key? key})
+      : super(key: key);
+  String text;
+  String image;
+  Function onTap;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 90,
+      width: 70,
+      margin: const EdgeInsets.all(8),
+      child: InkWell(
+        onTap: () {
+          onTap();
+        },
+        child: Column(
+          children: [
+            Container(
+              height: 70,
+              width: 70,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                    image: AssetImage(image), fit: BoxFit.cover),
+              ),
+            ),
+            Center(
+              child: Text(
+                text,
+                style: TextStyle(fontSize: 15, color: Colors.grey.shade800),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
