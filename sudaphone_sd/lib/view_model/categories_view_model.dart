491e2b0706e7d7b70896292e9adcce9f9c3827d3
3li-3bdullah <PROGRMMER7@gmail.com>
2022-04-17 22:25:38 +0200

    I've created new file and added firestore's var

---
+++ sudaphone_sd/lib/view_model/categories_view_model.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+class CategoriesViewModel extends GetxController{
+
+  var phonesCategory = FirebaseFirestore.instance.collection("phonesCategory");
+}
