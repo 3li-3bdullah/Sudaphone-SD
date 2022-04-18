52ec00ee509c56dc1f6cfe8486847a8321e7a69d
3li-3bdullah <PROGRMMER7@gmail.com>
2022-04-18 21:46:28 +0200

    I've created new class to get me data whenever i call it

---
+++ sudaphone_sd/lib/view/screen_widgets/all_products.dart
@@ -0,0 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:sudaphone_sd/view/screen_widgets/last_product.dart';
+import 'package:sudaphone_sd/view_model/screen_view_model.dart';
+
+// ignore: must_be_immutable
+class AllProducts extends GetWidget<ScreenViewModel> {
+  AllProducts(
+      {required this.collection,
+      required this.docOne,
+      required this.docTwo,
+      Key? key})
+      : super(key: key);
+  String docOne;
+  String docTwo;
+  String collection;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        future: controller.phonesCategory
+            .doc(docOne)
+            .collection(collection)
+            .doc(docTwo)
+            .get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return LastProduct(
+                imageProduct: snapshot.data?.data()!['imageUrl'],
+                text: snapshot.data?.data()!['name'],
+                price: snapshot.data?.data()!['price'],
+                collection: collection,
+                docOne: docOne,
+                snapshot: snapshot.data);
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: Lottie.asset("assets/lottie/please_wait.json"),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.purpleAccent,
+              ),
+            );
+          }
+        });
+  }
+}
