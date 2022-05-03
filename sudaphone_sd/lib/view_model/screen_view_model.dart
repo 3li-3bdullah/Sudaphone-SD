1075f3e0fd25a0744ad5020fb4d596aa1777754d
3li-3bdullah <PROGRMMER7@gmail.com>
2022-05-03 14:30:22 +0200

    I've declared three variables

--- sudaphone_sd/lib/view_model/screen_view_model.dart
+++ sudaphone_sd/lib/view_model/screen_view_model.dart
@@ -1,14 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ScreenViewModel extends GetxController {
-  //Declare Variables
+  /*
+  *Declare Variables 
+  */
   RxInt? activeIndex = 0.obs;
   RxDouble value = 0.0.obs;
   CollectionReference<Map<String, dynamic>> carouselFire = FirebaseFirestore.instance.collection("carousel");
   CollectionReference<Map<String, dynamic>> phonesCategory = FirebaseFirestore.instance.collection("phonesCategory");
-  FirebaseFirestore drawerData = FirebaseFirestore.instance;
+  String? uid;
+  FirebaseAuth auth = FirebaseAuth.instance;
+  CollectionReference<Map<String, dynamic>> drawerData = FirebaseFirestore.instance.collection("posts");
   Size size = MediaQuery.of(Get.context!).size;
   PageController controllerCarousel =
       PageController(viewportFraction: 0.8, keepPage: true);
@@ -22,33 +27,14 @@ class ScreenViewModel extends GetxController {
     Colors.purple,
     Colors.brown,
   ];
-   final List<String> docOne = const [
-    "allHuawei",
-    "allIphone",
-    "allOppo",
-    "allRealme",
-    "allXiaomi",
-    "allSamsung",
-    "allTecno",
-    "allNokia",
-    "allVivo",
-    "allLenovo"
-  ];
-  final List<String> collectionTwo = const [
-    "huawei",
-    "iphone",
-    "oppo",
-    "realme",
-    "xiaomi",
-    "samsung",
-    "tecno",
-    "nokia",
-    "vivo",
-    "lenovo"
-  ];
-
-
-
+  /* 
+  * Methods
+  */
+  @override
+  void onInit() {
+    uid = auth.currentUser!.uid;
+    super.onInit();
+  }
   void valueOne() {
     value(1.0);
   }
