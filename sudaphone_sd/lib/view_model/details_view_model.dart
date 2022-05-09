1c4e304d8ab787319d5b8c9d100b785a72400678
3li-3bdullah <PROGRMMER7@gmail.com>
2022-05-09 23:30:06 +0200

    I've added a lot of modified

--- sudaphone_sd/lib/view_model/details_view_model.dart
+++ sudaphone_sd/lib/view_model/details_view_model.dart
@@ -1,23 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class DetailsViewModel extends GetxController {
   /// variables
-  Size size = MediaQuery.of(Get.context!).size;
+  // Size size = MediaQuery.of(Get.context!).size;
   Rx<PanelController> panelController = PanelController().obs;
-  FirebaseAuth auth  = FirebaseAuth.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
   String? uid;
 
   /// Methods
 
-   @override
+  @override
   void onInit() {
-      uid = auth.currentUser!.uid;
+    uid = auth.currentUser!.uid;
     super.onInit();
   }
+
   // To open & close panel
   void togglePanel() {
     panelController.value.isPanelOpen
@@ -37,6 +37,7 @@ class DetailsViewModel extends GetxController {
         .doc(docOne)
         .collection(collection!)
         .doc(snapshot.id);
+    
 
     bool _isHasLiked = await _likeData
         .get()
@@ -48,10 +49,37 @@ class DetailsViewModel extends GetxController {
         'likesCount': _addLike.toInt(),
         'usersLiked.$uid': true,
       });
+     
     } else {
       int _removeLike = snapshot['likesCount'] - 1;
       await _likeData.update(
           {'likesCount': _removeLike.toInt(), 'usersLiked.$uid': false});
+      
     }
   }
+  //To add items at the archivists
+   Future<void> addToArchivists({archivistsSnapshot}) async {
+      DocumentReference<Map<String, dynamic>> archivists = FirebaseFirestore
+        .instance
+        .collection("posts")
+        .doc(uid)
+        .collection("archivists")
+        .doc(archivistsSnapshot.id);
+        bool? isThereSavedData = await archivists.get().then((data) => data.data()!['name'] != null);
+        isThereSavedData == null ?  await archivists.set({
+        'name': archivistsSnapshot['name'].toString(),
+        'imageUrl': archivistsSnapshot['imageUrl'].toString(),
+        'likesCount': archivistsSnapshot['likesCount'].toString(),
+        'ram': archivistsSnapshot['ram'].toString(),
+        'storage': archivistsSnapshot['storage'].toString(),
+        'battery': archivistsSnapshot['battery'].toString(),
+        'frontCamera': archivistsSnapshot['frontCamera'].toString(),
+        'rearCamera': archivistsSnapshot['rearCamera'].toString(),
+        'screen': archivistsSnapshot['screen'].toString(),
+        'os': archivistsSnapshot['os'].toString(),
+        'usersLiked': archivistsSnapshot['usersLiked'],
+        'price': archivistsSnapshot['price'].toString(),
+        'cpu': archivistsSnapshot['cpu'].toString(),
+      }) : await archivists.delete();
+    }
 }
