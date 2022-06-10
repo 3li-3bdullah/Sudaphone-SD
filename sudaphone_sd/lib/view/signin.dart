e169a84864ad259b6680e683ca91215280263f09
3li-3bdullah <PROGRMMER7@gmail.com>
2022-06-10 21:45:16 +0200

    I've seperated the class login into two class to be more readable

---
+++ sudaphone_sd/lib/view/signin.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sudaphone_sd/view/login_widgets/primary_button.dart';
+import 'package:sudaphone_sd/view/login_widgets/signin_form.dart';
+import 'package:sudaphone_sd/view/signup.dart';
+import 'package:sudaphone_sd/view/widgets/custom_text.dart';
+import 'package:sudaphone_sd/view_model/login_view_model.dart';
+
+class SignIn extends GetWidget<LoginViewModel> {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: double.maxFinite,
+          width: double.maxFinite,
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  height: MediaQuery.of(context).size.height / 3,
+                  width: MediaQuery.of(context).size.height / 1.5,
+                  child: Image.asset(
+                    "assets/images/login/login_logo.png",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const SignInForm(),
+                const SizedBox(
+                  height: 20,
+                ),
+                PrimaryButton(
+                  color: Colors.brown,
+                  buttonText: "Sign In",
+                  ontap: () => controller.signInWithEmailAndPassword(
+                      controller.emailController!.text,
+                      controller.passwordController!.text,
+                      controller.signInKey!),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const CustomText(
+                        text: "Don't have an acount ? ",
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                        textAlign: TextAlign.center),
+                    InkWell(
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            color: Colors.brown,
+                            decoration: TextDecoration.underline),
+                      ),
+                      onTap: () {
+                        Get.to(() => const SignUp(),
+                            transition: Transition.zoom,
+                            duration: const Duration(milliseconds: 300));
+                      },
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ sudaphone_sd/lib/view/signup.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sudaphone_sd/constants.dart';
+import 'package:sudaphone_sd/view/login_widgets/build_avatar.dart';
+import 'package:sudaphone_sd/view/login_widgets/primary_button.dart';
+import 'package:sudaphone_sd/view/login_widgets/signup_form.dart';
+import 'package:sudaphone_sd/view/signin.dart';
+import 'package:sudaphone_sd/view/widgets/custom_text.dart';
+import 'package:sudaphone_sd/view_model/login_view_model.dart';
+
+class SignUp extends GetWidget<LoginViewModel> {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: double.maxFinite,
+          width: double.maxFinite,
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // const SizedBox(height: 30,),
+                const BuildAvatar(),
+                const SizedBox(height: 30),
+                const SignUpForm(),
+                const SizedBox(
+                  height: 20,
+                ),
+                PrimaryButton(
+                  color: Colors.brown,
+                  buttonText: "Sign Up",
+                  ontap: () => controller.signUpWithEmailAndPassword(
+                      controller.usernameController!.text,
+                      controller.emailController!.text,
+                      controller.passwordController!.text,
+                      controller.signUpKey!),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const CustomText(
+                        text: "Do you have an acount ? ",
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                        textAlign: TextAlign.center),
+                    InkWell(
+                      child: const Text(
+                        "Sign In",
+                        style: TextStyle(
+                            color: Colors.brown,
+                            decoration: TextDecoration.underline),
+                      ),
+                      onTap: () {
+                        Get.to(() => const SignIn(),
+                            transition: Transition.zoom,
+                            duration: const Duration(milliseconds: 300));
+                      },
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
