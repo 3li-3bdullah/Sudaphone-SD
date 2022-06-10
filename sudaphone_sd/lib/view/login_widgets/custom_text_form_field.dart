147d4239c6325681f3598683e9a5e9aa70c5f0ef
3li-3bdullah <PROGRMMER7@gmail.com>
2022-06-10 21:51:47 +0200

    I've added some enhanced

--- sudaphone_sd/lib/view/login_widgets/custom_text_form_field.dart
+++ sudaphone_sd/lib/view/login_widgets/custom_text_form_field.dart
@@ -1,64 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:sudaphone_sd/constants.dart';
 
 class CustomTextFormField extends StatelessWidget {
   // ignore: use_key_in_widget_constructors
-  const CustomTextFormField({
-    required this.obscure,
-    required this.validator,
-    required this.icon,
-    required this.myhinttext,
-    // required this.fillColor,
-    // required this.enabledColor,
-    // required this.focusedColor,
-    // required this.borderSideColor,
-    required this.prefixColor,
-    required this.textEditingController,
-    required this.labelText
-  });
+  const CustomTextFormField(
+      {required this.obscure,
+      required this.validator,
+      required this.icon,
+      required this.myhinttext,
+      required this.prefixColor,
+      required this.textEditingController,
+      required this.labelText});
   final bool obscure;
   final Function validator;
   final IconData icon;
   final String myhinttext;
-  // final Color fillColor;
-  // final Color enabledColor;
-  // final Color focusedColor;
-  // final Color borderSideColor;
   final Color prefixColor;
   final String labelText;
   final TextEditingController textEditingController;
   @override
   Widget build(BuildContext context) {
-    return TextFormField(style: const TextStyle(color: Colors.white),
+    return TextFormField(
       controller: textEditingController,
-      
       obscureText: obscure,
-      validator:(value){
+      validator: (value) {
         return validator(value);
       },
       decoration: InputDecoration(
-        
-        labelText: labelText,labelStyle: const TextStyle(color: Colors.white ),
+        hintStyle: const TextStyle(color: kTextFieldColor),
+        focusedBorder: const UnderlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide(color: kPrimaryColor)),
+            enabledBorder : const OutlineInputBorder(
+            borderRadius:  BorderRadius.all(Radius.circular(10))
+            ),
+        labelText: labelText, labelStyle: const TextStyle(color: Colors.white),
         contentPadding: const EdgeInsets.all(4),
         // hintText: myhinttext,
-        prefixIcon: Icon(icon, color: Colors.white , size: 30,),
-        // filled: true,
-        // fillColor: fillColor,
-        
-        enabledBorder: const OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(50)),
-          borderSide: BorderSide(
-              color: Colors.white, style: BorderStyle.solid, width: 1),
+        prefixIcon: Icon(
+          icon,
+          color: Colors.brown,
+          size: 30,
         ),
-        focusedBorder:const OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(50)),
-          borderSide: BorderSide(
-              color: Colors.white, style: BorderStyle.solid, width: 1),
-        ),
-        border: const OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(50),),
-          borderSide: BorderSide(
-              color: Colors.white, style: BorderStyle.solid, width: 1),
-        ),
+        filled: true,
+        fillColor: kTextFieldColor,
       ),
     );
   }
