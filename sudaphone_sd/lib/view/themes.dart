43cd51dbed8f59c201c5426e2ee656a0f12f8923
3li-3bdullah <PROGRMMER7@gmail.com>
2022-06-17 23:58:02 +0200

    Added cupertinoswitch and some methods from themesviewmodel

---
+++ sudaphone_sd/lib/view/themes.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sudaphone_sd/view/widgets/custom_text.dart';
+import 'package:sudaphone_sd/view_model/themes_view_model.dart';
+
+class Themes extends GetWidget<ThemesViewModel> {
+  const Themes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const CustomText(
+        text: "Dark mode",
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
+      trailing: CupertinoSwitch(
+        value: Get.isDarkMode,
+        onChanged: (value) {
+          if (Get.isDarkMode) {
+            controller.changeThemeMode(ThemeMode.light);
+            controller.saveTheme(false);
+          } else {
+            controller.changeThemeMode(ThemeMode.dark);
+            controller.saveTheme(true);
+          }
+        },
+      ),
+    );
+  }
+}
