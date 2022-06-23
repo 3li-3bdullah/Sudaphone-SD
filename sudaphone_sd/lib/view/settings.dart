f76b4673047bdcbe6fc75b778f8f81ad788c478f
3li-3bdullah <PROGRMMER7@gmail.com>
2022-06-23 16:57:00 +0200

    I've created custom bottomSheet

--- sudaphone_sd/lib/view/settings.dart
+++ sudaphone_sd/lib/view/settings.dart
@@ -3,13 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone_sd/constants.dart';
 import 'package:sudaphone_sd/view/login_widgets/custom_text_form_field.dart';
+import 'package:sudaphone_sd/view/themes.dart';
 import 'package:sudaphone_sd/view/widgets/custom_text.dart';
 import 'package:sudaphone_sd/view/widgets/snack_to_upload_images.dart';
 import 'package:sudaphone_sd/view_model/settings_view_model.dart';
+import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
+// ignore: must_be_immutable
 class Settings extends GetWidget<SettingsViewModel> {
-  const Settings({Key? key}) : super(key: key);
-
+  Settings({Key? key}) : super(key: key);
+  var controll = Get.find<ThemesViewModel>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +23,7 @@ class Settings extends GetWidget<SettingsViewModel> {
           ),
           leading: IconButton(
             icon: const Icon(
-              Icons.arrow_back_ios,
+              Icons.arrow_back,
               color: Colors.black,
             ),
             onPressed: () {
@@ -35,7 +38,7 @@ class Settings extends GetWidget<SettingsViewModel> {
         child: Column(
           children: [
             FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                future: controller.getUserData.doc("${controller.uid}").get(),
+                future: controller.getUserData.doc(controller.uid.toString()).get(),
                 builder: (context, snapshot) {
                   return Column(
                     children: [
@@ -45,7 +48,7 @@ class Settings extends GetWidget<SettingsViewModel> {
                           child: CircleAvatar(
                             radius: 70,
                             backgroundImage: NetworkImage(
-                                "${snapshot.data?.data()?['profileUrl']}"),
+                                "${snapshot.data?.data()!['profileUrl']}"),
                           ),
                         ),
                         onTap: () {
@@ -61,7 +64,7 @@ class Settings extends GetWidget<SettingsViewModel> {
                       ),
                       InkWell(
                         child: CustomText(
-                          text: "${snapshot.data?.data()?['userName']}",
+                          text: "${snapshot.data?.data()!['userName']}",
                           color: Colors.black,
                           fontSize: 25,
                           fontWeight: FontWeight.bold,
@@ -116,30 +119,91 @@ class Settings extends GetWidget<SettingsViewModel> {
                     ],
                   );
                 }),
-            // ListTile(
-            //   title: const Text("The Mode"),
-            //   trailing: const Icon(Icons.wb_sunny),
-            //   onTap: () {
-            //     Get.bottomSheet(Column(
-            //       children: [
-            //         ListTile(
-            //           title: const Text("Light Mode"),
-            //           leading: const Icon(Icons.wb_sunny_outlined),
-            //           onTap: () {
-            //             Get.changeTheme(ThemeData.light());
-            //           },
-            //         ),
-            //         ListTile(
-            //           title: const Text("Dark Mode"),
-            //           leading: const Icon(Icons.wb_sunny),
-            //           onTap: () {
-            //             Get.changeTheme(ThemeData.dark());
-            //           },
-            //         ),
-            //       ],
-            //     ));
-            //   },
-            // ),
+            const SizedBox(height: 10),
+            InkWell(
+              child: const Themesx(),
+              onTap: () {
+                Get.bottomSheet(
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+                    height: MediaQuery.of(context).size.height / 2,
+                    decoration: const BoxDecoration(
+                      color: Colors.black54,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30)),
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Container(
+                          height: 5,
+                          width: 30,
+                          alignment: Alignment.center,
+                          decoration: const BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Row(
+                          children: [
+                            Radio(value: controller.dark, groupValue: controller.groupValue, onChanged: (value){
+                            controll.changeThemeMode(ThemeMode.dark);
+                                    controll.saveTheme(true);
+                          },activeColor: Colors.brown,fillColor: MaterialStateProperty.all(Colors.grey)),
+                           const Align(
+                              alignment: Alignment.centerLeft,
+                              child:  CustomText(text: "Dark mode ",textAlign: TextAlign.justify,color: Colors.white,fontSize: 20,fontWeight: FontWeight.normal,),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                       Row(
+                         children: [
+                          Radio(value: controller.light, groupValue: controller.groupValue, onChanged: (value){
+                            controll.changeThemeMode(ThemeMode.light);
+                                    controll.saveTheme(false);
+                          },activeColor: Colors.brown,fillColor: MaterialStateProperty.all(Colors.grey)),
+                          const Align(
+                              alignment: Alignment.centerLeft,
+                              child:  CustomText(text: "Light mode ",textAlign: TextAlign.justify,color: Colors.white,fontSize: 20,fontWeight: FontWeight.normal,),
+                            ),
+                         ],
+                       ),
+                      ],
+                    ),
+                  ),
+                );
+                //        if(Get.isDarkMode){
+                //         controll.changeThemeMode(ThemeMode.light);
+                // controll.saveTheme(false);
+                //        } else{
+                //         controll.changeThemeMode(ThemeMode.dark);
+                // controll.saveTheme(true);
+                //        }
+              },
+            ),
+            //     GetBuilder<ThemesViewModel>(
+            //       init: ThemesViewModel(),
+            //       builder:(context)=> ElevatedButton(onPressed: (){
+            //        if(Get.isDarkMode){
+            //         controll.changeThemeMode(ThemeMode.light);
+            // controll.saveTheme(false);
+            //        } else{
+            //         controll.changeThemeMode(ThemeMode.dark);
+            // controll.saveTheme(true);
+            //    }
+            //   }, child: const Text("Change Mode")),
+            // )
           ],
         ),
       ),
