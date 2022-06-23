6837886a8c5e5bad15e3b308408df5391f5986ad
3li-3bdullah <PROGRMMER7@gmail.com>
2022-06-23 17:02:44 +0200

    I've added update method to change the status

--- sudaphone_sd/lib/view_model/themes_view_model.dart
+++ sudaphone_sd/lib/view_model/themes_view_model.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
-class ThemesViewModel extends GetxController{
+class ThemesViewModel extends GetxController {
   final GetStorage _box = GetStorage();
   final String _key = "isDarkMode";
 
@@ -10,5 +10,8 @@ class ThemesViewModel extends GetxController{
   bool _loadTheme() => _box.read(_key) ?? false;
   void saveTheme(bool isDarkMode) => _box.write(_key, isDarkMode);
   void changeTheme(ThemeData theme) => Get.changeTheme(theme);
-  void changeThemeMode(ThemeMode theme) => Get.changeThemeMode(theme);
+  void changeThemeMode(ThemeMode theme) {
+     Get.changeThemeMode(theme);
+    update();
+  }
 }
