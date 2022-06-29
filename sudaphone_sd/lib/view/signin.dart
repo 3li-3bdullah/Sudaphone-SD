b57d28bb3f1ede2c069049553cf1cb59870ecf4d
3li-3bdullah <PROGRMMER7@gmail.com>
2022-06-29 07:56:23 +0200

    I've added obx with the loading var to show current page or loading page

--- sudaphone_sd/lib/view/signin.dart
+++ sudaphone_sd/lib/view/signin.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:sudaphone_sd/view/login_widgets/signin_form.dart';
 import 'package:sudaphone_sd/view/signup.dart';
 import 'package:sudaphone_sd/view/widgets/custom_text.dart';
+import 'package:sudaphone_sd/view/widgets/loading.dart';
 import 'package:sudaphone_sd/view/widgets/primary_button.dart';
 import 'package:sudaphone_sd/view_model/login_view_model.dart';
 
@@ -13,65 +14,74 @@ class SignIn extends GetWidget<LoginViewModel> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          height: double.maxFinite,
-          width: double.maxFinite,
-          alignment: Alignment.center,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
+        child: Obx(
+          () => controller.showLoading.value
+              ? Loading(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  lotHeight: MediaQuery.of(context).size.height / 3)
+              : Container(
+                  height: double.maxFinite,
+                  width: double.maxFinite,
                   alignment: Alignment.center,
-                  height: MediaQuery.of(context).size.height / 3,
-                  width: MediaQuery.of(context).size.height / 1.5,
-                  child: Image.asset(
-                    "assets/images/login/login_logo.png",
-                    fit: BoxFit.cover,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          height: MediaQuery.of(context).size.height / 3,
+                          width: MediaQuery.of(context).size.height / 1.5,
+                          child: Image.asset(
+                            "assets/images/login/login_logo.png",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        const SignInForm(),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        PrimaryButton(
+                          color: Colors.brown,
+                          buttonText: "Sign In",
+                          ontap: () => controller.signInWithEmailAndPassword(
+                              controller.emailSigninController!.text,
+                              controller.passwordSigninController!.text,
+                              controller.signInKey!
+                              ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const CustomText(
+                                text: "Don't have an acount ? ",
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                                textAlign: TextAlign.center),
+                            InkWell(
+                              child: const Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                    color: Colors.brown,
+                                    decoration: TextDecoration.underline),
+                              ),
+                              onTap: () {
+                                Get.to(() => const SignUp(),
+                                    transition: Transition.zoom,
+                                    duration:
+                                        const Duration(milliseconds: 300));
+                              },
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
-                const SizedBox(height: 30),
-                const SignInForm(),
-                const SizedBox(
-                  height: 20,
-                ),
-                PrimaryButton(
-                  color: Colors.brown,
-                  buttonText: "Sign In",
-                  ontap: () => controller.signInWithEmailAndPassword(
-                      controller.emailSigninController!.text,
-                      controller.passwordSigninController!.text,
-                      controller.signInKey!),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const CustomText(
-                        text: "Don't have an acount ? ",
-                        color: Colors.black,
-                        fontSize: 14,
-                        fontWeight: FontWeight.normal,
-                        textAlign: TextAlign.center),
-                    InkWell(
-                      child: const Text(
-                        "Sign Up",
-                        style: TextStyle(
-                            color: Colors.brown,
-                            decoration: TextDecoration.underline),
-                      ),
-                      onTap: () {
-                        Get.to(() => const SignUp(),
-                            transition: Transition.zoom,
-                            duration: const Duration(milliseconds: 300));
-                      },
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
         ),
       ),
     );
