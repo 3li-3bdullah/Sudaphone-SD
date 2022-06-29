f2ae374f2b1dbf64188f56acdd71657d5c9f09bd
3li-3bdullah <PROGRMMER7@gmail.com>
2022-06-29 07:43:21 +0200

    changed the color's of primary color

--- sudaphone_sd/lib/constants.dart
+++ sudaphone_sd/lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFF1B383A);
+const kPrimaryColor = Color(0xffd6a659);
 const kSecondaryColor = Color(0xFF59706F);
 const kDarkGreyColor = Color(0xFFA8A8A8);
 const kWhiteColor = Color(0xFFFFFFFF);
