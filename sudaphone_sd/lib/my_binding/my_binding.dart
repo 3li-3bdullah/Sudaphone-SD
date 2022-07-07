16210b9584a58c8390cc68bbb1740c83ac5f2dd1
3li-3bdullah <PROGRMMER7@gmail.com>
2022-07-07 23:53:17 +0200

    I've added the dependence of feedback

--- sudaphone_sd/lib/my_binding/my_binding.dart
+++ sudaphone_sd/lib/my_binding/my_binding.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:sudaphone_sd/view_model/categories_view_model.dart';
 import 'package:sudaphone_sd/view_model/details_view_model.dart';
 import 'package:sudaphone_sd/view_model/download_images_view_model.dart';
+import 'package:sudaphone_sd/view_model/feedback_view_model.dart';
 import 'package:sudaphone_sd/view_model/login_view_model.dart';
 import 'package:sudaphone_sd/view_model/main_view_model.dart';
 import 'package:sudaphone_sd/view_model/mydrawer_view_model.dart';
@@ -22,6 +23,7 @@ class MyBinding implements Bindings {
     Get.lazyPut(() => SettingsViewModel(), fenix: true);
     Get.lazyPut(() => DetailsViewModel(), fenix: true);
     Get.lazyPut(() => CategoriesViewModel(), fenix: true);
+    Get.lazyPut(() => FeedbackViewModel(), fenix: true);
     Get.put(ThemesViewModel());
   }
 }
