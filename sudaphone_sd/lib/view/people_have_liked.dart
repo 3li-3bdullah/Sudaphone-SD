e6b7a9c5b97a4b27dc565c00b0eb4747cea6f013
3li-3bdullah <PROGRMMER7@gmail.com>
2022-07-21 23:56:54 +0200

    I've changed the archticture with firebase

--- sudaphone_sd/lib/view/people_have_liked.dart
+++ sudaphone_sd/lib/view/people_have_liked.dart
@@ -1,14 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 import 'package:sudaphone_sd/view/widgets/custom_text.dart';
 import 'package:sudaphone_sd/view_model/posts_view_model.dart';
 
-// ignore: must_be_immutable
-class PeopleHaveLIked extends GetWidget<PostsViewModel> {
-  PeopleHaveLIked({required this.peopleWhoLiked,required this.currentDoc, Key? key}) : super(key: key);
-  var peopleWhoLiked;
-  var currentDoc;
+class PeopleHaveLiked extends GetWidget<PostsViewModel> {
+  const PeopleHaveLiked(
+      {required this.peopleWhoLiked, required this.currentDoc, Key? key})
+      : super(key: key);
+  final List<String> peopleWhoLiked;
+  final Map<String, dynamic> currentDoc;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,39 +29,63 @@ class PeopleHaveLIked extends GetWidget<PostsViewModel> {
           onPressed: () {
             Get.back();
           },
-          icon: const Icon(Icons.arrow_back , color: Colors.black,),
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
         ),
       ),
       body: ListView.builder(
         itemCount: peopleWhoLiked.length,
         itemBuilder: (context, index) {
           return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-            future: controller.postsCollections
-                .doc("${peopleWhoLiked[index]}")
-                .get(),
+            future:
+                controller.userInfoCollection.doc(peopleWhoLiked[index]).get(),
             builder: ((context, snapshot) {
-              if(currentDoc['${peopleWhoLiked[index]}']){
-                return Padding(
-                padding: const EdgeInsets.only(left: 2,top: 10),
-                child: ListTile(
-                  title: Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: CustomText(
-                        text: "${snapshot.data?.data()!['userName']}",
-                        fontSize: 17,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.black,
-                        textAlign: TextAlign.left),
-                  ),
-                  // leading: CircleAvatar(
-                  //   radius: 30,
-                  //   backgroundImage:
-                  //       NetworkImage(snapshot.data?.data()!['profileUrl']),
-                  // ),
-                ),
-              );
+              if (snapshot.hasData) {
+                if (currentDoc[peopleWhoLiked[index]]) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 2, top: 10),
+                    child: ListTile(
+                      title: Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: CustomText(
+                            text: "${snapshot.data?.data()!['userName']}",
+                            fontSize: 17,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black,
+                            textAlign: TextAlign.left),
+                      ),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            "${snapshot.data!.data()!['profileUrl']}"),
+                      ),
+                    ),
+                  );
+                } else {
+                  return const SizedBox();
+                }
+              } else if (snapshot.hasError) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.green,
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: CustomText(
+                      text: snapshot.error.toString(),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      textAlign: TextAlign.center),
+                );
               } else {
-                return const SizedBox();
+                return Container(
+                  alignment: Alignment.center,
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Lottie.asset("assets/lotties/loading.json"),
+                );
               }
             }),
           );
