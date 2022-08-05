f2dee7b627e6e7fb862395e345bb961172005874
3li-3bdullah <PROGRMMER7@gmail.com>
2022-08-05 23:42:54 +0200

    Created custom leading

---
+++ sudaphone_sd/lib/view/widgets/leading.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sudaphone_sd/constants.dart';
+import 'package:sudaphone_sd/view_model/themes_view_model.dart';
+
+class Leading extends StatelessWidget {
+  const Leading({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<ThemesViewModel>(
+          builder: (control) => IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color:
+                  control.theme == ThemeMode.dark ? Colors.white : kDarkColor,
+            ),
+            onPressed: () {
+              Get.back();
+            },
+          ),
+        );
+  }
+}
