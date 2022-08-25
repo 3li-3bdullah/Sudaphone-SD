306af13d5700311f20ddc1d779ba3c05e27cc67f
3li-3bdullah <PROGRMMER7@gmail.com>
2022-08-25 23:48:26 +0200

    I've enchanced the page style

--- sudaphone_sd/lib/view/feedbacks.dart
+++ sudaphone_sd/lib/view/feedbacks.dart
@@ -1,94 +1,136 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone_sd/constants.dart';
-import 'package:sudaphone_sd/view/widgets/loading.dart';
+import 'package:sudaphone_sd/view/widgets/custom_text.dart';
 import 'package:sudaphone_sd/view/widgets/primary_button.dart';
 import 'package:sudaphone_sd/view_model/feedback_view_model.dart';
+import 'package:sudaphone_sd/view_model/public_data.dart';
+import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
 class Feedbacks extends GetWidget<FeedbackViewModel> {
   const Feedbacks({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Feedback",
-          style: TextStyle(color: Colors.black),
+        title: CustomText(
+          text: "Feedback",
+          fontSize: 20,
+          fontWeight: FontWeight.normal,
+          textAlign: TextAlign.center,
         ),
-        centerTitle: true,
-        backgroundColor: Colors.white,
+        leading: GetBuilder<ThemesViewModel>(
+            builder: (control) => IconButton(
+                  onPressed: () {
+                    Get.back();
+                  },
+                  icon: const Icon(Icons.arrow_back),
+                  color: control.theme == ThemeMode.dark
+                      ? Colors.white
+                      : Colors.black,
+                )),
+        elevation: 0,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: const [
-                CircleAvatar(
-                  child: Text(
-                    "S-SD",
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
-                  ),
-                  backgroundColor: Colors.brown,
+      body: Container(
+        alignment: Alignment.center,
+        height: size.height,
+        width: size.width,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // const SizedBox(height: 30),
+              Padding(
+                padding: EdgeInsets.only(left: size.width / 4.7),
+                child: Image.asset(
+                  "assets/images/send_feedback.png",
+                  fit: BoxFit.contain,
+                  height:size.height / 3,
+                  width: size.width,
                 ),
-                SizedBox(width: 20),
-                Text(
-                  "Sudaphone SD",
-                  style: TextStyle(
-                      color: Colors.brown, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Form(
-              key: controller.textKey,
-              child: TextFormField(
-                controller: controller.textEditingController,
-                validator: (note) {
-                  if (note!.trim().isEmpty) {
-                    return "The field is empty";
-                  }
-                },
-                cursorColor: Colors.brown,
-                minLines: 5,
-                decoration: const InputDecoration(
-                  focusedBorder: UnderlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10),
+              ),
+              const SizedBox(height: 10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  CircleAvatar(
+                    radius: 35,
+                    backgroundColor: Colors.brown,
+                    child: Text(
+                      "S-SD",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
                     ),
-                    borderSide: BorderSide(color: kPrimaryColor),
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10),
+                  SizedBox(width: 20),
+                  Text(
+                    "Sudaphone SD",
+                    style: TextStyle(
+                        color: Colors.brown,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Form(
+                  key: controller.textKey,
+                  child: TextFormField(
+                    controller: controller.textEditingController,
+                    validator: (note) {
+                      if (note!.trim().isEmpty) {
+                        return "The field is empty";
+                      }
+                    },
+                    minLines: 3,
+                    maxLines: 5,
+                    cursorColor: Colors.brown,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: kTextFieldColor,
+                      hintStyle: TextStyle(color: kTextFieldColor),
+                      focusedBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          borderSide: BorderSide(color: kPrimaryColor)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      contentPadding: EdgeInsets.all(4),
                     ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            controller.showLoading.value
-                ? Loading(
-                    height: MediaQuery.of(context).size.width / 3,
-                    width: MediaQuery.of(context).size.width,
-                    lotHeight: MediaQuery.of(context).size.width / 3)
-                : PrimaryButton(
-                    buttonText: "Send Feedback",
-                    ontap: () {
-                      controller.sendFeedback(
-                        key: controller.textKey,
-                        text: controller.textEditingController.text.toString(),
-                      );
-                    },
-                    color: Colors.brown,
-                  )
-          ],
+              const SizedBox(
+                height: 40,
+              ),
+              // controller.showLoading.value
+              //     ? Loading(
+              //         height: MediaQuery.of(context).size.width / 3,
+              //         width: MediaQuery.of(context).size.width,
+              //         lotHeight: MediaQuery.of(context).size.width / 3)
+              //     : 
+                  GetBuilder<PublicData>(
+                      builder: (control) => PrimaryButton(
+                        buttonText: "Send Feedback",
+                        ontap: () {
+                          controller.sendFeedback(
+                            uid: control.uid,
+                            name: control.getUserName,
+                            profileUrl: control.getProfileUrl,
+                            key: controller.textKey,
+                            text: controller.textEditingController.text
+                                .toString(),
+                          );
+                        },
+                        color: Colors.brown,
+                      ),
+                    )
+            ],
+          ),
         ),
       ),
     );
