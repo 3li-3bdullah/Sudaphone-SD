d42112f3b1512fa8501c7c922a6e4c8d521fd450
3li-3bdullah <PROGRMMER7@gmail.com>
2022-08-26 22:57:21 +0200

    Added new custom text for details

---
+++ sudaphone_sd/lib/view/details_widget/custom_text_details.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomText2 extends StatelessWidget {
+  const CustomText2({
+    Key? key,
+    required this.text,
+    this.textAlign,
+    this.fontSize,
+    this.fontWeight,
+    required this.color,
+  }) : super(key: key);
+  final String text;
+  final TextAlign? textAlign;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: textAlign!,
+      style:
+          TextStyle(color: color, fontSize: fontSize!, fontWeight: fontWeight),
+    );
+  }
+}
