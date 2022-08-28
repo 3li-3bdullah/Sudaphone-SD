41edc5c969d5948910a4c5ea1cba617e47e17d29
3li-3bdullah <PROGRMMER7@gmail.com>
2022-08-28 23:30:06 +0200

    I used ThemesViewModel to change the theme

--- sudaphone_sd/lib/view/screen_widgets/build_images_carousel.dart
+++ sudaphone_sd/lib/view/screen_widgets/build_images_carousel.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sudaphone_sd/constants.dart';
+import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
 class BuildImagesCarousel extends StatelessWidget {
   const BuildImagesCarousel({Key? key, required this.imagesCarousel})
@@ -7,17 +10,19 @@ class BuildImagesCarousel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 5),
-      color: Colors.white,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(
-            image: NetworkImage(
-              imagesCarousel,
+    return GetBuilder<ThemesViewModel>(
+      builder: (controller) => Container(
+        margin: const EdgeInsets.symmetric(horizontal: 5),
+        color: controller.theme == ThemeMode.dark ? kDarkColor : Colors.white,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              image: NetworkImage(
+                imagesCarousel,
+              ),
+              fit: BoxFit.cover,
             ),
-            fit: BoxFit.cover,
           ),
         ),
       ),
