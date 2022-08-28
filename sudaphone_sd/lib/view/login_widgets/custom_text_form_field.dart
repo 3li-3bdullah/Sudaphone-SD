d82c6c8adf8b762dd50e99bf0504df5c5dac9e4b
3li-3bdullah <PROGRMMER7@gmail.com>
2022-08-28 23:15:21 +0200

    Added enhances

--- sudaphone_sd/lib/view/login_widgets/custom_text_form_field.dart
+++ sudaphone_sd/lib/view/login_widgets/custom_text_form_field.dart
@@ -5,17 +5,15 @@ class CustomTextFormField extends StatelessWidget {
   // ignore: use_key_in_widget_constructors
   const CustomTextFormField(
       {required this.obscure,
-      this.istrue = false, 
-      this.changeObscureValue, 
+      this.istrue = false,
+      this.changeObscureValue,
       required this.validator,
       required this.icon,
       required this.textEditingController,
-       this.customHintText = ""});
+      this.customHintText = ""});
   final bool obscure;
   final Function validator;
   final IconData icon;
-  // final String myhinttext;
-  // final Color prefixColor;
   final String customHintText;
   final bool istrue;
   final Function? changeObscureValue;
@@ -27,42 +25,38 @@ class CustomTextFormField extends StatelessWidget {
       obscureText: obscure ? istrue : false,
       validator: (value) {
         return validator(value);
-      },cursorColor: Colors.brown,
+      },
+      cursorColor: Colors.brown,
       decoration: InputDecoration(
-        
         hintStyle: const TextStyle(color: kTextFieldColor),
         focusedBorder: const UnderlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
             borderSide: BorderSide(color: kPrimaryColor)),
-            enabledBorder : const OutlineInputBorder(
-            borderRadius:  BorderRadius.all(Radius.circular(10))
-            ),
+        enabledBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10))),
         hintText: customHintText,
-        //  labelStyle: const TextStyle(color: Colors.white),
         contentPadding: const EdgeInsets.all(4),
-        // hintText: myhinttext,
-        
         prefixIcon: Icon(
           icon,
           color: Colors.white,
           size: 30,
         ),
-         suffixIcon: obscure
-                ? IconButton(
-                    onPressed: () {
-                     changeObscureValue!();
-                    },
-                    icon: istrue
-                        ? const Icon(
-                            Icons.visibility_off,
-                            color: Colors.brown,
-                          )
-                        : const Icon(
-                            Icons.visibility,
-                            color: Colors.white,
-                          ),
-                  )
-                : const SizedBox(),
+        suffixIcon: obscure
+            ? IconButton(
+                onPressed: () {
+                  changeObscureValue!();
+                },
+                icon: istrue
+                    ? const Icon(
+                        Icons.visibility_off,
+                        color: Colors.brown,
+                      )
+                    : const Icon(
+                        Icons.visibility,
+                        color: Colors.white,
+                      ),
+              )
+            : const SizedBox(),
         filled: true,
         fillColor: kTextFieldColor,
       ),
