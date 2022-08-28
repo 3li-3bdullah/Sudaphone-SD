817e3e78cd28f927e4c114e3d766b8152d8f340b
3li-3bdullah <PROGRMMER7@gmail.com>
2022-08-28 23:35:17 +0200

    Removed the betterPlayer from here

--- sudaphone_sd/lib/view/screen_widgets/custom_listtile.dart
+++ sudaphone_sd/lib/view/screen_widgets/custom_listtile.dart
@@ -1,41 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sudaphone_sd/view/screen_pages/play_video_page.dart';
+import 'package:sudaphone_sd/view/widgets/custom_text.dart';
+import 'package:sudaphone_sd/view_model/screen_view_model.dart';
+import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
-// ignore: must_be_immutable
-class CustomListTile extends StatelessWidget {
-  CustomListTile(
-      {required this.text, required this.image, required this.onTap, Key? key})
+class CustomListTile extends GetWidget<ScreenViewModel> {
+  const CustomListTile(
+      {required this.text,
+      required this.videoUrl,
+      required this.logo,
+      required this.thumbinalUrl,
+      required this.onTap,
+      Key? key})
       : super(key: key);
-  String text;
-  String image;
-  Function onTap;
+  final String text;
+  final String thumbinalUrl;
+  final String videoUrl;
+  final String logo;
+  final Function onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 90,
-      width: 80,
-      margin: const EdgeInsets.all(8),
-      child: InkWell(
-        onTap: () {
-          onTap();
-        },
-        child: Column(
-          children: [
-            Container(
-              height: 80,
-              width: 80,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                image: DecorationImage(
-                    image: AssetImage(image), fit: BoxFit.cover),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GetBuilder<ThemesViewModel>(
+        builder: (control) => Card(
+          shadowColor:
+              control.theme == ThemeMode.dark ? Colors.white12 : Colors.black54,
+          child: Column(
+            children: [
+              InkWell(
+                onTap: () {
+                  Get.to(() => PlayVideoPage(videoUrl: videoUrl));
+                },
+                child: Stack(alignment: Alignment.center, children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height / 6,
+                    width: MediaQuery.of(context).size.width / 2.5,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(thumbinalUrl),
+                            fit: BoxFit.cover),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10))),
+                  ),
+                  const Positioned(
+                      child: CircleAvatar(
+                          radius: 20,
+                          backgroundColor: Colors.white54,
+                          child: Icon(
+                            Icons.play_arrow_outlined,
+                            color: Colors.black,
+                          )))
+                ]),
               ),
-            ),
-            Center(
-              child: Text(
-                text,
-                style: TextStyle(fontSize: 15, color: Colors.grey.shade800),
+              const SizedBox(height: 3),
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.all(10),
+                child: InkWell(
+                  onTap: () {
+                    onTap();
+                  },
+                  child: Row(
+                    children: [
+                      CustomText(
+                        text: text,
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 85,
+                        width: 85,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              image: NetworkImage(logo), fit: BoxFit.cover),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
