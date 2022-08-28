2e9446fdc36f12327fbd24f088037de164c89eb5
3li-3bdullah <PROGRMMER7@gmail.com>
2022-08-28 23:22:25 +0200

    Added some features

--- sudaphone_sd/lib/view/posts_pages/write_post.dart
+++ sudaphone_sd/lib/view/posts_pages/write_post.dart
@@ -1,10 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
-import 'package:sudaphone_sd/constants.dart';
 import 'package:get/get.dart';
+import 'package:sudaphone_sd/constants.dart';
+import 'package:sudaphone_sd/view/widgets/custom_text.dart';
+import 'package:sudaphone_sd/view/widgets/leading.dart';
 import 'package:sudaphone_sd/view_model/posts_view_model.dart';
+import 'package:sudaphone_sd/view_model/public_data.dart';
 
 class WritePost extends GetWidget<PostsViewModel> {
   const WritePost({Key? key}) : super(key: key);
@@ -13,60 +13,44 @@ class WritePost extends GetWidget<PostsViewModel> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Create Post",
-          style: TextStyle(color: Colors.black),
+        title: CustomText(
+          text: "Create Post",
+          fontSize: 20,
+          fontWeight: FontWeight.normal,
+          textAlign: TextAlign.center,
         ),
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Colors.black,
-          ),
-          onPressed: () {
-            Get.back();
-          },
-        ),
-        backgroundColor: Colors.white,
-        centerTitle: true,
+        leading: const Leading(),
         elevation: 0,
       ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
             children: [
-              FutureBuilder<DocumentSnapshot<Map<String,dynamic>>>(
-                future: controller.userInfoCollection.doc(FirebaseAuth.instance.currentUser!.uid).get(),
-                builder: (context,snapshot){
-               if(snapshot.hasData){
-                   return Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(left: 8, top: 20),
-                    child:  CircleAvatar(
-                      backgroundImage:  NetworkImage("${snapshot.data!.data()?['profileUrl']}"),
+              GetBuilder<PublicData>(
+                builder: (control) => Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(left: 8, top: 20),
+                      child: CircleAvatar(
+                        backgroundImage:
+                            NetworkImage("${control.getProfileUrl}"),
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 15),
-                    child:  Text(
-                      "${snapshot.data?.data()!['userName']}",
-                      style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
+                    const SizedBox(
+                      width: 15,
                     ),
-                  ),
-                ],
-              );
-               
-               }else{
-                return Lottie.asset("assets/lotties/loading.json");
-               } }
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: Text(control.getUserName.toString(),
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                  ],
                 ),
+              ),
               Container(
                 margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
                 child: Form(
@@ -80,11 +64,12 @@ class WritePost extends GetWidget<PostsViewModel> {
                         minLines: 5,
                         maxLines: 10,
                         controller: controller.textController,
+                        cursorColor: Colors.brown,
                         decoration: InputDecoration(
                           hintText: "Write here ...",
                           filled: true,
                           isDense: true,
-                          fillColor: Colors.grey.shade200,
+                          fillColor: kTextFieldColor,
                           contentPadding: const EdgeInsets.all(5),
                           focusedBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(5),
@@ -102,10 +87,13 @@ class WritePost extends GetWidget<PostsViewModel> {
                           children: const [
                             Icon(Icons.camera_alt_outlined,
                                 color: Colors.pink, size: 25),
-                                SizedBox(width: 10,),
+                            SizedBox(
+                              width: 10,
+                            ),
                             Text(
                               "From Camera",
-                              style: TextStyle(fontSize: 15, color: Colors.grey),
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.grey),
                             ),
                           ],
                         ),
@@ -119,10 +107,13 @@ class WritePost extends GetWidget<PostsViewModel> {
                           children: const [
                             Icon(Icons.photo_outlined,
                                 color: Colors.green, size: 25),
-                                SizedBox(width: 10,),
+                            SizedBox(
+                              width: 10,
+                            ),
                             Text(
                               "From Gallery",
-                              style: TextStyle(fontSize: 15, color: Colors.grey),
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.grey),
                             ),
                           ],
                         ),
@@ -131,21 +122,28 @@ class WritePost extends GetWidget<PostsViewModel> {
                         },
                       ),
                       const SizedBox(height: 15),
-                      InkWell(
-                        child: Container(
-                          alignment: Alignment.center,
-                          height: MediaQuery.of(context).size.height * 0.08,
-                          width: MediaQuery.of(context).size.width / 1.5,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16), color: Colors.brown),
-                          child: Text(
-                            "POST",
-                            style: textButton.copyWith(color: kWhiteColor),
+                      GetBuilder<PublicData>(
+                        builder: (control) => InkWell(
+                          child: Container(
+                            alignment: Alignment.center,
+                            height: MediaQuery.of(context).size.height * 0.08,
+                            width: MediaQuery.of(context).size.width / 1.5,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(16),
+                                color: Colors.brown),
+                            child: Text(
+                              "POST",
+                              style: textButton.copyWith(color: kWhiteColor),
+                            ),
+                          ),
+                          onTap: () => controller.addPost(
+                            username: control.getUserName,
+                            profileurl: control.getProfileUrl,
+                            text: controller.textController!.text,
+                            textFieldkey: controller.postKey,
                           ),
                         ),
-                        onTap: ()=>  controller.addPost(controller.textController!.text , controller.postKey,),
-                      )
-                      
+                      ),
                     ],
                   ),
                 ),
