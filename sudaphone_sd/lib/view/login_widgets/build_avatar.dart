b1d765e7b13590695c44746fe4b8142db6b79e86
3li-3bdullah <PROGRMMER7@gmail.com>
2022-08-28 23:14:40 +0200

    Added Get.back , so that will remove the dialog when user click on it

--- sudaphone_sd/lib/view/login_widgets/build_avatar.dart
+++ sudaphone_sd/lib/view/login_widgets/build_avatar.dart
@@ -10,109 +10,97 @@ class BuildAvatar extends GetWidget<LoginViewModel> {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<LoginViewModel>(
-      builder: ((controller) => controller.imageFile != null
-          ? SizedBox(
-              height: 100,
-              width: 100,
-              child: InkWell(
-                onTap: () {},
-                child: AvatarGlow(shape: BoxShape.circle,animate: true,
-                  child: CircleAvatar(
-                    radius: 100,
-                    backgroundColor: Colors.brown,
-                    backgroundImage: FileImage(controller.imageFile!),
-                  ),
-                  duration: const Duration(milliseconds: 2500),
-                  showTwoGlows: true,
-                  glowColor: Colors.brown,
-                  repeat: true,
-                  endRadius: 100,
-                  repeatPauseDuration: const Duration(milliseconds: 150),
-                ),
+      builder:(controller) => InkWell(
+            child: controller.imageFile != null ? AvatarGlow(
+              shape: BoxShape.circle,
+              animate: true,
+              showTwoGlows: true,
+              glowColor: Colors.brown,
+              repeat: true,
+              endRadius: 80,
+              repeatPauseDuration: const Duration(milliseconds: 50),
+              child: CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.brown,
+                backgroundImage: FileImage(controller.imageFile!),
               ),
-            )
-          : SizedBox(
-              height: 100,
-              width: 100,
-              child: AvatarGlow(
-                duration: const Duration(milliseconds: 2500),
-                  showTwoGlows: true,
-                  glowColor: Colors.brown,
-                  repeat: true,
-                  endRadius: 300,
-                  // repeatPauseDuration: const Duration(milliseconds: 150),
-                child: InkWell(
-                  onTap: () {
-                    Get.defaultDialog(
-                      content: const CustomText(
-                        text: "Choose an image from : ",
-                        textAlign: TextAlign.center,
-                        color: Colors.grey,
-                        fontSize: 16,
-                        fontWeight: FontWeight.normal,
-                      ),
-                      title: "Upload",
-                      cancel: MaterialButton(
-                        color: Colors.brown,
-                        elevation: 0,
-                        onPressed: () {
-                          controller.uploadProfilePic(source: "gallery");
-                        },
-                        child: Row(
-                          children: const [
-                            Icon(
-                              Icons.photo,
-                              color: Colors.white,
-                            ),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            CustomText(
-                              text: "Gallery",
-                              color: Colors.white,
-                              fontSize: 17,
-                              fontWeight: FontWeight.normal,
-                              textAlign: TextAlign.center,
-                            )
-                          ],
-                        ),
-                      ),
-                      confirm: MaterialButton(
-                        color: Colors.brown,
-                        elevation: 0,
-                        onPressed: () {
-                          controller.uploadProfilePic(source: "camera");
-                        },
-                        child: Row(
-                          children: const [
-                            Icon(
-                              Icons.camera_alt_outlined,
-                              color: Colors.white,
-                            ),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            CustomText(
-                              text: "Camera",
-                              color: Colors.white,
-                              fontSize: 17,
-                              fontWeight: FontWeight.normal,
-                              textAlign: TextAlign.center,
-                            )
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                  child: const CircleAvatar(
-                    radius: 100,
-                    backgroundColor: Colors.grey,
-                    child: Icon(Icons.person, size: 80, color: Colors.brown),
-                  ),
-                ),
-                 
+            ) : const AvatarGlow(
+              shape: BoxShape.circle,
+              animate: true,
+              
+              showTwoGlows: true,
+              glowColor: Colors.brown,
+              repeat: true,
+              endRadius: 80,
+              repeatPauseDuration:  Duration(milliseconds: 50),
+              child: CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.brown,
+                child: Icon(Icons.person, size: 60, color: Colors.white),
               ),
-            )),
+            ),
+            onTap: () {
+                Get.defaultDialog(
+                  content:  CustomText(
+                    text: "Choose an image from : ",
+                    textAlign: TextAlign.center,
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  title: "Upload",
+                  cancel: MaterialButton(
+                    color: Colors.brown,
+                    elevation: 0,
+                    onPressed: () {
+                      controller.uploadProfilePic(source: "gallery");
+                      Get.back();
+                    },
+                    child: Row(
+                      children:  [
+                        const Icon(
+                          Icons.photo,
+                          color: Colors.white,
+                        ),
+                       const SizedBox(
+                          width: 5,
+                        ),
+                        CustomText(
+                          text: "Gallery",
+                          fontSize: 17,
+                          fontWeight: FontWeight.normal,
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
+                  ),
+                  confirm: MaterialButton(
+                    color: Colors.brown,
+                    elevation: 0,
+                    onPressed: () {
+                      controller.uploadProfilePic(source: "camera");
+                      Get.back();
+                    },
+                    child: Row(
+                      children:  [
+                       const Icon(
+                          Icons.camera_alt_outlined,
+                          color: Colors.white,
+                        ),
+                       const SizedBox(
+                          width: 5,
+                        ),
+                        CustomText(
+                          text: "Camera",
+                          fontSize: 17,
+                          fontWeight: FontWeight.normal,
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
+                  ),
+                );
+            },
+          )
     );
   }
 }
