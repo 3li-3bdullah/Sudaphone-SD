3f574146db83f5f45b96df7de33775820b8cbb4f
3li-3bdullah <PROGRMMER7@gmail.com>
2022-08-29 18:17:19 +0200

    Removed unnecessary widgets

--- sudaphone_sd/lib/view_model/download_images_view_model.dart
+++ sudaphone_sd/lib/view_model/download_images_view_model.dart
@@ -4,16 +4,11 @@ import 'package:get/get.dart';
 
 class DownloadImagesViewModel extends GetxController {
 
-  void saveNetworkImage(String path) async {
+  void saveNetworkImage(String path) {
     GallerySaver.saveImage(path, albumName: "Sudaphone SD")
         .whenComplete(
           () => Get.showSnackbar(
             const GetSnackBar(
-              backgroundGradient: LinearGradient(
-                colors: [Colors.pinkAccent, Colors.purpleAccent],
-                begin: Alignment.bottomCenter,
-                end: Alignment.topCenter,
-              ),
               messageText: Text(
                 "Image saved successfully ^_^ enjoy dude",
                 style: TextStyle(color: Colors.white),
@@ -37,29 +32,5 @@ class DownloadImagesViewModel extends GetxController {
             duration: const Duration(seconds: 3),
           ),
         );
-
-    // .then(
-    //   (success) {
-    //     // CustomSnakbar.showSnakBar(
-    //     //       backgroundColor: Colors.white12,
-    //     //       context: Get.context,
-    //     //       message: "Image saved successfully",
-    //     //       title: "Done");
-    //     Get.showSnackbar(const GetSnackBar(
-    //       messageText: Text(
-    //         "Image saved successfully ^_^ enjoy dude",
-    //         style: TextStyle(color: Colors.black),
-    //       ),
-    //       titleText: Text(
-    //         "Done",
-    //         style: TextStyle(color: Colors.black),
-    //       ),
-    //       duration: Duration(seconds: 2),
-    //       snackPosition: SnackPosition.BOTTOM,
-    //       reverseAnimationCurve: Curves.easeInCubic,
-    //       backgroundColor: Colors.white12,
-    //     ),);
-    //   },
-    // );
   }
 }
