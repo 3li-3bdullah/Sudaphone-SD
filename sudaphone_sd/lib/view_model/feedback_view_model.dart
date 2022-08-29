742d5c691c25f88bd0e693e08128f33e9fd6be17
3li-3bdullah <PROGRMMER7@gmail.com>
2022-08-29 18:29:54 +0200

    Edited the user data

--- sudaphone_sd/lib/view_model/feedback_view_model.dart
+++ sudaphone_sd/lib/view_model/feedback_view_model.dart
@@ -1,46 +1,56 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:lottie/lottie.dart';
 
 class FeedbackViewModel extends GetxController {
+// ((((((((((((((((((((((( Variables )))))))))))))))))))))))
+
   TextEditingController textEditingController = TextEditingController();
   GlobalKey textKey = GlobalKey<FormState>();
-  String? uId;
-  Map<String, dynamic>? uInfo;
   final date = DateFormat('M/d/y - kk:mm').format(DateTime.now());
-  RxBool showLoading = false.obs;
+  Size size = MediaQuery.of(Get.context!).size;
+
+// ((((((((((((((((((((((((((( Methods )))))))))))))))))))))))))))
 
   @override
   void onInit() {
-    uId = FirebaseAuth.instance.currentUser!.uid;
-    getUserInfo();
     textEditingController = TextEditingController();
     super.onInit();
   }
 
   @override
   void onClose() {
-    textEditingController.clear();
+    textEditingController.dispose();
     super.onClose();
   }
 
-  getUserInfo() async {
-    uInfo = await FirebaseFirestore.instance
-        .collection("usersInfo")
-        .doc(uId.toString())
-        .get()
-        .then((value) => value.data());
-  }
-
-  sendFeedback({GlobalKey? key, String? text}) async {
-    showLoading.value = true;
+  sendFeedback(
+      {GlobalKey? key,
+      String? text,
+      String? uid,
+      String? name,
+      String? profileUrl}) async {
+    showDialog(
+      barrierDismissible: false,
+      context: Get.context!,
+      builder: (BuildContext context) => Center(
+        child: Lottie.asset("assets/lotties/loading.json"),
+      ),
+    );
     await FirebaseFirestore.instance.collection("Feedbacks").add({
       "Note": text.toString(),
-      "Sender Name": uInfo!['username'].toString(),
-      "Sender Id": uId.toString(),
+      "Sender Name": name.toString(),
+      "Sender Id": uid.toString(),
       "Time": date.toString(),
-    }).whenComplete(() => showLoading.value = false);
+      "profileUrl": profileUrl.toString()
+    }).whenComplete(() => Get.back());
+    Get.snackbar("", "Your Feedback Has sent successfully",
+        duration: const Duration(seconds: 4),
+        backgroundColor: Colors.greenAccent,
+        snackPosition: SnackPosition.BOTTOM,
+        colorText: Colors.white);
+    textEditingController.clear();
   }
 }
