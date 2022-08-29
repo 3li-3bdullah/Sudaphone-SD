87caf3bf0dc6776f7b130084f2276ed95982880c
3li-3bdullah <PROGRMMER7@gmail.com>
2022-08-29 18:15:42 +0200

    just one var

--- sudaphone_sd/lib/view_model/categories_view_model.dart
+++ sudaphone_sd/lib/view_model/categories_view_model.dart
@@ -1,7 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
-class CategoriesViewModel extends GetxController{
-
+class CategoriesViewModel extends GetxController {
   var phonesCategory = FirebaseFirestore.instance.collection("phonesCategory");
+
+
 }
