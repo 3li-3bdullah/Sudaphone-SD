7df73476b7f094ef970c1133039562ad66bfe407
3li-3bdullah <PROGRMMER7@gmail.com>
2022-08-29 18:14:45 +0200

    I've removed some unnecessary widgets

--- sudaphone_sd/lib/view/themes.dart
+++ sudaphone_sd/lib/view/themes.dart
@@ -3,7 +3,6 @@ import 'dart:developer';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sudaphone_sd/view/widgets/custom_text.dart';
 import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
 class Themesx extends GetWidget<ThemesViewModel> {
@@ -16,23 +15,19 @@ class Themesx extends GetWidget<ThemesViewModel> {
         title: const Text(
           "Dark mode",
           style: TextStyle(
-              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+              color: Colors.black, fontSize: 15, fontWeight: FontWeight.bold),
         ),
         trailing: CupertinoSwitch(
           value: Get.isDarkMode,
           onChanged: (value) {
             log(value.toString());
             value = Get.isDarkMode;
-            log("=============");
-            log(value.toString());
             if (Get.isDarkMode) {
               controller.changeThemeMode(ThemeMode.dark);
               controller.saveTheme(true);
-              log("dark theme");
             } else {
               controller.changeThemeMode(ThemeMode.light);
               controller.saveTheme(false);
-              log("light theme");
             }
           },
         ),
