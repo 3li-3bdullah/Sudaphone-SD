d5ac6fad3cb9d922d93b7d54db05abbe3fbb1e2e
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-02 23:13:05 +0200

    Added the screen model inside a home folder

---
+++ sudaphone_sd/lib/model/home/screen_model.dart
@@ -0,0 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class MainCarousel {
+  MainCarousel({required this.imageUrl});
+  final String imageUrl;
+
+  MainCarousel.fromJson(QueryDocumentSnapshot<Map<String, dynamic>> json)
+      : imageUrl = json.data()['imageUrl'];
+}
+
+class LastProducts {
+  LastProducts(
+      {required this.docId, required this.imageUrl, required this.name});
+  final String docId;
+  final String imageUrl;
+  final String name;
+
+  LastProducts.fromJson(QueryDocumentSnapshot<Map<String, dynamic>> json)
+      : docId = json.id.toString(),
+        imageUrl = json.data()['imageUrl'] as String,
+        name = json.data()['name'] as String;
+}
+
+// Best Gaming Phones
+class BGPhones {
+  final String name;
+  final String image;
+  final String docId;
+
+  BGPhones({required this.name, required this.image, required this.docId});
+  BGPhones.fromJson(QueryDocumentSnapshot<Map<String, dynamic>> json)
+      : name = json['name'],
+        image = json['imageUrl'],
+        docId = json.id.toString();
+}
+
+class Category {
+  final String videoUrl;
+  final String thumbinalUrl;
+  final String logo;
+  final String name;
+
+  Category(
+      {required this.videoUrl,
+      required this.thumbinalUrl,
+      required this.logo,
+      required this.name});
+
+  Category.fromJson(QueryDocumentSnapshot<Map<String, dynamic>> json)
+      : videoUrl = json['video'],
+        thumbinalUrl = json['videoImage'],
+        name = json['name'],
+        logo = json['logo'];
+}
