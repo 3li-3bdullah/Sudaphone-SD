31bdf2deed7bc48c810e3efb43e97b446ad7d0dd
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-22 22:57:20 +0200

    Added the function from class searach_future_builder and i've made it as a component for home screens ;))

---
+++ sudaphone_sd/lib/view/home/components/search_result.dart
@@ -0,0 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:sudaphone_sd/shared/components/phone_list.dart';
+
+class SearchResult extends StatelessWidget {
+  const SearchResult({
+    Key? key,
+    required this.query,
+    required this.collection1,
+    required this.doc1,
+    required this.collection2,
+  }) : super(key: key);
+  final String query;
+  final String collection1;
+  final String doc1;
+  final String collection2;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        future: FirebaseFirestore.instance
+            .collection(collection1)
+            .doc(doc1)
+            .collection(collection2)
+            .orderBy('name')
+            .startAt([query]).endAt([query + '\uf8ff']).get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data?.docs.length,
+                itemBuilder: (context, index) {
+                  return PhoneList(
+                    isHasData: snapshot,
+                    collction: collection2,
+                    snapshot: snapshot.data?.docs[index],
+                    docOne: doc1,
+                  );
+                });
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: Image.asset("assets/images/loader.gif"),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.brown,
+              ),
+            );
+          }
+        });
+  }
+}
