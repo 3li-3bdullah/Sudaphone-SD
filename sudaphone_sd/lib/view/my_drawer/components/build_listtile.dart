7bb3ac3e11a2a502a6bb765b5c28c4a6e64512ec
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-25 19:42:31 +0200

    I've moved the widget build_listtile to my_drawer\components

---
+++ sudaphone_sd/lib/view/my_drawer/components/build_listtile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class BuildListTile extends StatelessWidget {
+  // ignore: use_key_in_widget_constructors
+  const BuildListTile({
+    required this.text,
+    required this.onTap,
+    required this.icon,
+  });
+  final String text;
+  final Function onTap;
+  final String icon;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: ListTile(
+        title: Text(
+          text,
+          style: const TextStyle(color: Colors.white, fontSize: 18),
+        ),
+        leading: Image(
+          image: AssetImage(icon),
+          color: Colors.white,
+          height: 25,
+          width: 25,
+        ),
+      ),
+      onTap: () {
+        onTap();
+      },
+    );
+  }
+}
