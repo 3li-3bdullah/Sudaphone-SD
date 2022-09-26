425661021170288ce2bacc3ef836aee1d0562db5
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-27 01:03:12 +0200

    Wrapped the card with ClipRRect with 30 borderRadius and increased the Container radius

--- sudaphone_sd/lib/view/home/components/custom_listtile.dart
+++ sudaphone_sd/lib/view/home/components/custom_listtile.dart
@@ -21,72 +21,100 @@ class CustomListTile extends GetWidget<ScreenViewModel> {
   final Function onTap;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: GetBuilder<ThemesViewModel>(
-        builder: (control) => Card(
-          shadowColor:
-              control.theme == ThemeMode.dark ? Colors.white12 : Colors.black54,
-          child: Column(
-            children: [
-              InkWell(
-                onTap: () {
-                  Get.to(() => PlayVideoPage(videoUrl: videoUrl));
-                },
-                child: Stack(alignment: Alignment.center, children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height / 6,
-                    width: MediaQuery.of(context).size.width / 2.5,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: NetworkImage(thumbinalUrl),
-                            fit: BoxFit.cover),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10))),
-                  ),
-                  const Positioned(
-                      child: CircleAvatar(
-                          radius: 20,
-                          backgroundColor: Colors.white54,
-                          child: Icon(
-                            Icons.play_arrow_outlined,
-                            color: Colors.black,
-                          )))
-                ]),
-              ),
-              const SizedBox(height: 3),
-              Container(
-                alignment: Alignment.center,
-                margin: const EdgeInsets.all(10),
-                child: InkWell(
-                  onTap: () {
-                    onTap();
-                  },
-                  child: Row(
-                    children: [
-                      CustomText(
-                        text: text,
-                        fontSize: 14,
-                        fontWeight: FontWeight.normal,
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Container(
-                        height: 85,
-                        width: 85,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                              image: NetworkImage(logo), fit: BoxFit.cover),
+    return GetBuilder<ThemesViewModel>(
+      builder: (control) => Container(
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+            color: control.theme == ThemeMode.dark
+                ? Colors.white12
+                : Colors.black12,
+            blurRadius: 3.0,
+          ),
+        ], borderRadius: const BorderRadius.all(Radius.circular(20))),
+        // margin: const EdgeInsets.all(3.0),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(30)),
+          child: Card(
+            shadowColor:
+                control.theme == ThemeMode.dark ? Colors.white12 : Colors.black12,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Get.to(() => PlayVideoPage(videoUrl: videoUrl));
+                    },
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height / 5,
+                          width: MediaQuery.of(context).size.width / 2.5,
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            child: FadeInImage.assetNetwork(
+                              placeholder: 'assets/images/loader.gif',
+                              image: thumbinalUrl,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
-                      ),
-                    ],
+                        const Positioned(
+                          child: CircleAvatar(
+                            radius: 20,
+                            backgroundColor: Colors.white54,
+                            child: Icon(
+                              Icons.play_arrow_outlined,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
+                  const SizedBox(height: 3),
+                  Container(
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.all(10),
+                    child: InkWell(
+                      onTap: () {
+                        onTap();
+                      },
+                      child: Row(
+                        children: [
+                          CustomText(
+                            text: text,
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          SizedBox(
+                            height: 90,
+                            width: 90,
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                              child: FadeInImage.assetNetwork(
+                                placeholder: 'assets/images/loader.gif',
+                                image: logo,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
