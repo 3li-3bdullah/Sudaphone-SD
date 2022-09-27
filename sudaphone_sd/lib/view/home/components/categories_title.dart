d10f2ac3e3321a1d39ae49427124b5840006304e
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-27 23:34:29 +0200

    I've added underlined shape by Container , so the screen became beautifullest

--- sudaphone_sd/lib/view/home/components/categories_title.dart
+++ sudaphone_sd/lib/view/home/components/categories_title.dart
@@ -1,46 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone_sd/shared/components/custom_text.dart';
+import 'package:sudaphone_sd/shared/components/custom_text2.dart';
 import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
 class CategoriesTitle extends StatelessWidget {
- const CategoriesTitle({Key? key, this.text, this.press}) : super(key: key);
+  const CategoriesTitle(
+      {Key? key, this.text, this.press, required this.underLineWidget})
+      : super(key: key);
 
   final String? text;
   final Function? press;
+  final double underLineWidget;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: Row(children: [
-        GetBuilder<ThemesViewModel>(
-          builder: (controller) => Container(
-            decoration:  BoxDecoration(
-                color: controller.theme == ThemeMode.dark? Colors.white12 : Colors.black12,
-                borderRadius: const BorderRadius.all(Radius.circular(10))),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: CustomText(
-                text: text!,
-                fontSize: 18,
-                fontWeight: FontWeight.normal,
-                textAlign: TextAlign.center,
-              ),
+      child: Row(
+        children: [
+          GetBuilder<ThemesViewModel>(
+            builder: (controller) => Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: CustomText2(
+                    text: text!,
+                    fontSize: 18,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  height: 4,
+                  width: underLineWidget,
+                  alignment: Alignment.centerLeft,
+                  decoration: const BoxDecoration(
+                      color: Colors.brown,
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                )
+              ],
             ),
           ),
-        ),
-        const Spacer(),
-        IconButton(
-            // shape:
-            // RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-            onPressed: () {
-              press!();
-            },
-            icon: const Icon(
-              Icons.arrow_forward,
-              size: 30,
-            ))
-      ]),
+          const Spacer(),
+          TextButton(
+              onPressed: () {
+                press!();
+              },
+              child: CustomText2(
+                color: Colors.brown.shade300,
+                text: "View All",
+                fontSize: 13,
+                fontWeight: FontWeight.bold,
+                textAlign: TextAlign.center,
+              )),
+        ],
+      ),
     );
   }
 }
