ab89ae2e1fe52a889663c78f8a33ee08f32ea8c8
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-28 23:50:31 +0200

    I've called the controller themesviewmodel the control the bottomnav color

--- sudaphone_sd/lib/view/bottom_nav_bar/bottom_navigation.dart
+++ sudaphone_sd/lib/view/bottom_nav_bar/bottom_navigation.dart
@@ -1,19 +1,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sudaphone_sd/shared/constants.dart';
 import 'package:sudaphone_sd/view/categories/categories.dart';
 import 'package:sudaphone_sd/view/posts/posts.dart';
 import 'package:sudaphone_sd/view/home/screen.dart';
 import 'package:sudaphone_sd/view/settings/settings.dart';
 import 'package:sudaphone_sd/view_model/screen_view_model.dart';
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
 
 // ignore: must_be_immutable
 class BottomNavigation extends GetWidget<ScreenViewModel> {
   BottomNavigation({Key? key}) : super(key: key);
   final List<Widget> screens = [
-     const Screen(),
+      Screen(),
     const Categories(),
      const Posts(),
     Setting(),
@@ -23,50 +25,53 @@ class BottomNavigation extends GetWidget<ScreenViewModel> {
     return GetX<ScreenViewModel>(
       init: ScreenViewModel(),
       builder: (controller) => Scaffold(
-        bottomNavigationBar: BottomNavyBar(
-          selectedIndex: controller.activeIndex.value,
-          showElevation: true,
-          onItemSelected: (index) {
-            controller.activeIndex.value = index;
-          },
-          items: [
-            BottomNavyBarItem(
-                icon: const Image(
-                  image: AssetImage("assets/images/icons/home.png"),
-                  color: Colors.red,
-                  height: 25,
-                  width: 25,
-                ),
-                title: const Text("Home"),
-                activeColor: Colors.red),
-            BottomNavyBarItem(
-                icon: const Image(
-                  image: AssetImage("assets/images/icons/category.png"),
-                  color: Colors.purpleAccent,
-                  height: 25,
-                  width: 25,
-                ),
-                title: const Text("Categories"),
-                activeColor: Colors.purpleAccent),
-            BottomNavyBarItem(
-                icon: const Image(
-                  image: AssetImage("assets/images/icons/add.png"),
-                  color: Colors.pink,
-                  height: 25,
-                  width: 25,
-                ),
-                title: const Text("Posts"),
-                activeColor: Colors.pink),
-            BottomNavyBarItem(
-                icon: const Image(
-                  image: AssetImage("assets/images/icons/settings.png"),
-                  color: Colors.blue,
-                  height: 25,
-                  width: 25,
-                ),
-                title: const Text("Settings"),
-                activeColor: Colors.blue),
-          ],
+        bottomNavigationBar: GetBuilder<ThemesViewModel>(
+          builder:(control) => BottomNavyBar(
+            selectedIndex: controller.activeIndex.value,
+            showElevation: true,
+            backgroundColor: control.theme == ThemeMode.dark ? kDarkColor : kBackgroundColor,
+            onItemSelected: (index) {
+              controller.activeIndex.value = index;
+            },
+            items: [
+              BottomNavyBarItem(
+                  icon: const Image(
+                    image: AssetImage("assets/images/icons/home.png"),
+                    color: Colors.red,
+                    height: 25,
+                    width: 25,
+                  ),
+                  title: const Text("Home"),
+                  activeColor: Colors.red),
+              BottomNavyBarItem(
+                  icon: const Image(
+                    image: AssetImage("assets/images/icons/category.png"),
+                    color: Colors.purpleAccent,
+                    height: 25,
+                    width: 25,
+                  ),
+                  title: const Text("Categories"),
+                  activeColor: Colors.purpleAccent),
+              BottomNavyBarItem(
+                  icon: const Image(
+                    image: AssetImage("assets/images/icons/add.png"),
+                    color: Colors.pink,
+                    height: 25,
+                    width: 25,
+                  ),
+                  title: const Text("Posts"),
+                  activeColor: Colors.pink),
+              BottomNavyBarItem(
+                  icon: const Image(
+                    image: AssetImage("assets/images/icons/settings.png"),
+                    color: Colors.blue,
+                    height: 25,
+                    width: 25,
+                  ),
+                  title: const Text("Settings"),
+                  activeColor: Colors.blue),
+            ],
+          ),
         ),
         body: screens[controller.activeIndex.value],
       ),
