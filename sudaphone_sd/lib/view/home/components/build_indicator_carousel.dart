0277ababb6af04e6fc0e3b36031e2f315429fb69
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-29 22:33:29 +0200

    Added some editing

--- sudaphone_sd/lib/view/home/components/build_indicator_carousel.dart
+++ sudaphone_sd/lib/view/home/components/build_indicator_carousel.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone_sd/view_model/screen_view_model.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
 class BuildIndicatorCarousel extends GetWidget<ScreenViewModel> {
   const BuildIndicatorCarousel({required this.imageLength, Key? key})
@@ -9,43 +10,45 @@ class BuildIndicatorCarousel extends GetWidget<ScreenViewModel> {
   final int imageLength;
   @override
   Widget build(BuildContext context) {
-    return SmoothPageIndicator(
-      controller: controller.controllerCarousel,
-      count: imageLength,
-      effect: CustomizableEffect(
-        activeDotDecoration: DotDecoration(
-          width: 32,
-          height: 12,
-          color: Colors.white,
-          rotationAngle: 180,
-          verticalOffset: -10,
-          borderRadius: BorderRadius.circular(24),
-          dotBorder: const DotBorder(
-            padding: 2,
-            width: 2,
-            color: Colors.black,
+    return GetBuilder<ThemesViewModel>(
+      builder: (control) => SmoothPageIndicator(
+        controller: controller.controllerCarousel,
+        count: imageLength,
+        effect: CustomizableEffect(
+          activeDotDecoration: DotDecoration(
+            width: 32,
+            height: 12,
+            color: Colors.white,
+            rotationAngle: 180,
+            verticalOffset: -10,
+            borderRadius: BorderRadius.circular(24),
+            dotBorder: DotBorder(
+              padding: 2,
+              width: 2,
+              color: control.theme == ThemeMode.dark ? Colors.brown : Colors.black26,
+            ),
           ),
-        ),
-        dotDecoration: const DotDecoration(
-          width: 24,
-          height: 12,
-          color: Colors.white,
-          dotBorder: DotBorder(
-            padding: 2,
-            width: 2,
-            color: Colors.black26,
+          dotDecoration: DotDecoration(
+            width: 24,
+            height: 12,
+            color: Colors.white,
+            dotBorder: DotBorder(
+              padding: 2,
+              width: 2,
+              color: control.theme == ThemeMode.dark ? Colors.grey : Colors.black26,
+            ),
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(2),
+                topRight: Radius.circular(16),
+                bottomLeft: Radius.circular(16),
+                bottomRight: Radius.circular(2)),
+            //borderRadius: BorderRadius.circular(16),
+            verticalOffset: 0,
           ),
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(2),
-              topRight: Radius.circular(16),
-              bottomLeft: Radius.circular(16),
-              bottomRight: Radius.circular(2)),
-          //borderRadius: BorderRadius.circular(16),
-          verticalOffset: 0,
+          spacing: 6.0,
+          // activeColorOverride: (i) => colors[i],
+          inActiveColorOverride: (i) => controller.colors[i],
         ),
-        spacing: 6.0,
-        // activeColorOverride: (i) => colors[i],
-        inActiveColorOverride: (i) => controller.colors[i],
       ),
     );
   }
