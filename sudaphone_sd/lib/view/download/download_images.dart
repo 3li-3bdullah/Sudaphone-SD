ff89a93ada29e21d7601db6ae656794d23c5019e
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-29 22:30:01 +0200

    I've changed the color and enhanced the code by removed some widgets

--- sudaphone_sd/lib/view/download/download_images.dart
+++ sudaphone_sd/lib/view/download/download_images.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone_sd/view_model/download_images_view_model.dart';
 
-// ignore: must_be_immutable
 class DownloadImages extends GetWidget<DownloadImagesViewModel> {
   const DownloadImages({Key? key, required this.image}) : super(key: key);
   final String image;
@@ -10,29 +9,28 @@ class DownloadImages extends GetWidget<DownloadImagesViewModel> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: GetBuilder<DownloadImagesViewModel>(
-          builder: (controller) => FloatingActionButton.extended(
-            backgroundColor: Colors.grey,
-            onPressed: () {
-              controller.saveNetworkImage(image);
-            },
-            label: const Icon(Icons.download_outlined,
-                size: 20, color: Colors.white),
+      floatingActionButton: GetBuilder<DownloadImagesViewModel>(
+        builder: (controller) => FloatingActionButton.extended(
+          backgroundColor: Colors.brown,
+          onPressed: () {
+            controller.saveNetworkImage(image);
+          },
+          label: const Icon(Icons.download_outlined,
+              size: 20, color: Colors.white),
+        ),
+      ),
+      body: SafeArea(
+        child: Container(
+          color: Colors.black,
+          alignment: Alignment.center,
+          width: double.maxFinite,
+          height: MediaQuery.of(context).size.height,
+          child: SingleChildScrollView(
+            child: FadeInImage.assetNetwork(
+                placeholder: 'assets/images/loader.gif', image: image),
           ),
         ),
-        body: SafeArea(
-          child: Container(
-            color: Colors.black,
-            alignment: Alignment.center,
-            width: double.maxFinite,
-            height: MediaQuery.of(context).size.height,
-            child: SingleChildScrollView(
-              child: Image(
-                image: NetworkImage(image),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        ));
+      ),
+    );
   }
 }
