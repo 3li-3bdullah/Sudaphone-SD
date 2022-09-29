e504265a46a3a08b4bf5582e6ef4479a8c762305
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-29 10:24:50 +0200

    I've made the class extends ThemesViewModel & Added inside Container to boxShadow with different colors and removed the variable text

--- sudaphone_sd/lib/view/categories/components/categories_card.dart
+++ sudaphone_sd/lib/view/categories/components/categories_card.dart
@@ -1,45 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:sudaphone_sd/shared/components/custom_text.dart';
+import 'package:get/get.dart';
+import 'package:sudaphone_sd/shared/constants.dart';
+import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
-class CategoriesCard extends StatelessWidget {
+class CategoriesCard extends GetWidget<ThemesViewModel> {
   // ignore: use_key_in_widget_constructors
   const CategoriesCard({
     required this.imageCat,
-    required this.text,
     required this.onTap,
   });
   final String imageCat;
-  final String text;
   final Function onTap;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(5),
       child: InkWell(
-          child: Card(
-              elevation: 3,
-              child: SizedBox(
-                height: 170,
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: Image.asset(
-                        imageCat,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    CustomText(
-                      text: text,
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          decoration: BoxDecoration(
+            borderRadius:const BorderRadius.all(Radius.circular(10)),
+            boxShadow: [
+              BoxShadow(
+                  color: controller.theme == ThemeMode.dark ? Colors.black38 : Colors.black12,
+                   blurRadius: 6.0, offset: const Offset(8, 8)),
+             const BoxShadow(
+                  color: Colors.transparent,
+                  blurRadius: 6.0,
+                  offset: Offset(-8, -8)),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+            child: GetBuilder<ThemesViewModel>(
+              builder: (control) => Card(
+                color: control.theme == ThemeMode.light
+                    ? Colors.white
+                    : kDarkColor,
+                child: SizedBox(
+                  height: 170,
+                  child: Image.asset(
+                    imageCat,
+                    fit: BoxFit.fill,
+                  ),
                 ),
-              )),
-          onTap: () {
-            onTap();
-          }),
+              ),
+            ),
+          ),
+        ),
+        onTap: () {
+          onTap();
+        },
+      ),
     );
   }
 }
