4e61d04498b95095560964f0c5459ee76f934c44
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-29 23:09:48 +0200

    Added Custom Inkwell to call it everywhere at posts

---
+++ sudaphone_sd/lib/view/posts/components/choose_image_icon.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:sudaphone_sd/shared/components/custom_text.dart';
+
+class ChooseImageIcon extends StatelessWidget {
+  const ChooseImageIcon({Key? key, required this.text, required this.ontap, required this.icon, required this.color}) : super(key: key);
+  final String text;
+  final Function ontap;
+  final IconData icon;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Row(
+        children: [
+         Icon(icon, color: color, size: 25),
+         const SizedBox(
+            width: 10,
+          ),
+          CustomText(
+            text: text,
+            fontSize: 15,
+            fontWeight: FontWeight.normal,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+      onTap: () {ontap(build(context));},
+    );
+  }
+}
