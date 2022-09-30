7025ae0cfcc238560f7c43ad71bee6c526390907
3li-3bdullah <PROGRMMER7@gmail.com>
2022-09-30 17:47:21 +0200

    Added new variable to make the text View All visible or not

--- sudaphone_sd/lib/view/home/components/categories_title.dart
+++ sudaphone_sd/lib/view/home/components/categories_title.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sudaphone_sd/shared/components/custom_text.dart';
 import 'package:sudaphone_sd/shared/components/custom_text2.dart';
+import 'package:sudaphone_sd/shared/components/custom_title.dart';
 import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
 class CategoriesTitle extends StatelessWidget {
   const CategoriesTitle(
-      {Key? key, this.text, this.press, required this.underLineWidget})
+      {Key? key, this.text, this.press, required this.underLineWidget, this.showViewAll = false})
       : super(key: key);
 
   final String? text;
   final Function? press;
   final double underLineWidget;
+  final bool showViewAll;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -19,31 +20,10 @@ class CategoriesTitle extends StatelessWidget {
       child: Row(
         children: [
           GetBuilder<ThemesViewModel>(
-            builder: (controller) => Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: CustomText2(
-                    text: text!,
-                    fontSize: 18,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Container(
-                  height: 4,
-                  width: underLineWidget,
-                  alignment: Alignment.centerLeft,
-                  decoration: const BoxDecoration(
-                      color: Colors.brown,
-                      borderRadius: BorderRadius.all(Radius.circular(5))),
-                )
-              ],
-            ),
-          ),
+              builder: (controller) =>
+                  CustomTitle(text: text!, underLineWidget: underLineWidget)),
           const Spacer(),
-          TextButton(
+      showViewAll ?  TextButton(
               onPressed: () {
                 press!();
               },
@@ -53,7 +33,7 @@ class CategoriesTitle extends StatelessWidget {
                 fontSize: 13,
                 fontWeight: FontWeight.bold,
                 textAlign: TextAlign.center,
-              )),
+              )) : const SizedBox(),
         ],
       ),
     );
