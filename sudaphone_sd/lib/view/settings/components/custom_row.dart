ed95f0f8b05630e50cc97b0e3dfd07c3baf5a3e6
3li-3bdullah <PROGRMMER7@gmail.com>
2022-10-01 15:01:39 +0200

    Wrapped the Padding with InkWell and declared new var of type function ;)

--- sudaphone_sd/lib/view/settings/components/custom_row.dart
+++ sudaphone_sd/lib/view/settings/components/custom_row.dart
@@ -5,31 +5,35 @@ import 'package:get/get.dart';
 import 'package:sudaphone_sd/shared/components/custom_text.dart';
 
 class CustomRow extends StatelessWidget {
-  const CustomRow({Key? key, required this.name, required this.imagePath}) : super(key: key);
+  const CustomRow({Key? key, required this.name, required this.imagePath, required this.onTap}) : super(key: key);
   final String name;
   final String imagePath;
+  final Function onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          SizedBox(
-              height: Get.height / 20,
-              child: Image.asset(imagePath)),
-          const SizedBox(
-            width: 20,
-          ),
-          CustomText(
-            text: name,
-            fontSize: 18,
-            fontWeight: FontWeight.normal,
-            textAlign: TextAlign.center,
-          ),
-          const Spacer(),
-          const Icon(Icons.arrow_forward_ios)
-        ],
+    return InkWell(
+      onTap: (){onTap();},
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            SizedBox(
+                height: Get.height / 20,
+                child: Image.asset(imagePath)),
+            const SizedBox(
+              width: 20,
+            ),
+            CustomText(
+              text: name,
+              fontSize: 18,
+              fontWeight: FontWeight.normal,
+              textAlign: TextAlign.center,
+            ),
+            const Spacer(),
+            const Icon(Icons.arrow_forward_ios)
+          ],
+        ),
       ),
     );
   }
