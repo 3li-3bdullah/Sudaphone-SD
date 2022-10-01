303e8b72ee7ef8cb845f384cc8b13679ef1902ae
3li-3bdullah <PROGRMMER7@gmail.com>
2022-10-01 14:34:13 +0200

    I've used the custom settings profile

--- sudaphone_sd/lib/view/settings/settings.dart
+++ sudaphone_sd/lib/view/settings/settings.dart
@@ -7,6 +7,7 @@ import 'package:sudaphone_sd/shared/components/custom_text2.dart';
 import 'package:sudaphone_sd/shared/components/custom_text_form_field.dart';
 import 'package:sudaphone_sd/shared/components/custom_title.dart';
 import 'package:sudaphone_sd/view/login/signin.dart';
+import 'package:sudaphone_sd/view/settings/components/settings_profile.dart';
 import 'package:sudaphone_sd/view_model/public_data.dart';
 import 'package:sudaphone_sd/view_model/settings_view_model.dart';
 import 'package:sudaphone_sd/view_model/themes_view_model.dart';
@@ -28,94 +29,7 @@ class Setting extends GetWidget<SettingsViewModel> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            // const SizedBox(height: 20,),
-            GetBuilder<PublicData>(
-              builder: (control) => StreamBuilder<
-                      DocumentSnapshot<Map<String, dynamic>>>(
-                  stream: controller.getUserData.doc(control.uid).snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Column(
-                        children: [
-                          Container(
-                            height: size.height / 4,
-                            width: size.width,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image: NetworkImage(
-                                      "${snapshot.data!.data()!['profileUrl']}"),
-                                  fit: BoxFit.cover,
-                                  opacity: 0.5),
-                              borderRadius: const BorderRadius.all(
-                                Radius.circular(10),
-                              ),
-                            ),
-                            margin: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 10),
-                            padding:
-                                const EdgeInsets.only(left: 10.0, right: 10.0),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  flex: 1,
-                                  child: CircleAvatar(
-                                    radius: 70,
-                                    backgroundImage: NetworkImage(
-                                        "${snapshot.data!.data()!['profileUrl']}"),
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 20,
-                                ),
-                                Expanded(
-                                  flex: 2,
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      const SizedBox(
-                                        height: 5,
-                                      ),
-                                      CustomText(
-                                        text:
-                                            "${snapshot.data!.data()!['userName']}",
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.normal,
-                                        textAlign: TextAlign.center,
-                                      ),
-                                      const SizedBox(
-                                        height: 10,
-                                      ),
-                                      CustomText(
-                                        text: snapshot.data!.data()?['email'],
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.normal,
-                                        textAlign: TextAlign.center,
-                                      ),
-                                      const SizedBox(
-                                        height: 10,
-                                      ),
-                                      CustomText(
-                                        text: snapshot.data!
-                                            .data()?['registerTime'],
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.normal,
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ],
-                      );
-                    } else {
-                      return Image.asset("assets/images/loader.gif");
-                    }
-                  }),
-            ),
+            const SettingsProfile(),
             InkWell(
               onTap: () {
                 Get.defaultDialog(
