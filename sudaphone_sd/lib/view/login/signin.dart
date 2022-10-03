8be2c42042286056016784fffc9cfdaadfa9dedf
3li-3bdullah <PROGRMMER7@gmail.com>
2022-10-03 22:14:41 +0200

    Added custom container and column to enahce the UI

--- sudaphone_sd/lib/view/login/signin.dart
+++ sudaphone_sd/lib/view/login/signin.dart
@@ -16,117 +16,132 @@ class SignIn extends GetWidget<LoginViewModel> {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Container(
-                  height: double.maxFinite,
-                  width: double.maxFinite,
-                  alignment: Alignment.center,
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Container(
-                            alignment: Alignment.centerLeft,
-                            padding: const EdgeInsets.only(left: 20),
-                            child: const CustomText2(
-                                text: "Sudaphone SD",
-                                textAlign: TextAlign.left,
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.brown)),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          alignment: Alignment.center,
-                          height: MediaQuery.of(context).size.height / 2.8,
-                          width: MediaQuery.of(context).size.width,
-                          child: Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              Container(
-                                height: controller.size.height / 2.8,
-                                width: controller.size.width,
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 20, vertical: 20),
-                                decoration: const BoxDecoration(
-                                    color: Colors.brown,
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(20))),
-                              ),
-                              Image.asset(
-                                "assets/images/login/login_logo.png",
-                                fit: BoxFit.cover,
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 30),
-                        const SignInForm(),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        PrimaryButton(
+          height: double.maxFinite,
+          width: double.maxFinite,
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Column(
+                    children: [
+                      const CustomText2(
+                          text: "Sudaphone SD",
+                          textAlign: TextAlign.left,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.brown),
+                      Container(
+                        height: 4,
+                        width: 160,
+                        alignment: Alignment.centerLeft,
+                        decoration: const BoxDecoration(
                           color: Colors.brown,
-                          buttonText: "Sign In",
-                          ontap: () => controller.signInWithEmailAndPassword(
-                              controller.emailSigninController!.text,
-                              controller.passwordSigninController!.text,
-                              controller.signInKey!),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Get.to(
-                              () => const ForgotPassword(),
-                              transition: Transition.zoom,
-                            );
-                          },
-                          child: const Text(
-                            "Forgot Password",
-                            style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              color: Colors.brown,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            textAlign: TextAlign.center,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
                           ),
                         ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const CustomText2(
-                                text: "Don't have an acount ? ",
-                                color: Colors.black,
-                                fontSize: 14,
-                                fontWeight: FontWeight.normal,
-                                textAlign: TextAlign.center),
-                            InkWell(
-                              child: const Text(
-                                "Sign Up",
-                                style: TextStyle(
-                                    color: Colors.brown,
-                                    decoration: TextDecoration.underline),
-                              ),
-                              onTap: () {
-                                Get.to(() => const SignUp(),
-                                    transition: Transition.zoom,
-                                    duration:
-                                        const Duration(milliseconds: 300));
-                              },
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                      ],
-                    ),
+                      )
+                    ],
                   ),
                 ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  height: MediaQuery.of(context).size.height / 2.8,
+                  width: MediaQuery.of(context).size.width,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        height: controller.size.height / 2.8,
+                        width: controller.size.width,
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 20),
+                        decoration: const BoxDecoration(
+                            color: Colors.brown,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                      ),
+                      Image.asset(
+                        "assets/images/login/login_logo.png",
+                        fit: BoxFit.cover,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const SignInForm(),
+                const SizedBox(
+                  height: 20,
+                ),
+                PrimaryButton(
+                  color: Colors.brown,
+                  buttonText: "Sign In",
+                  ontap: () => controller.signInWithEmailAndPassword(
+                      controller.emailSigninController!.text,
+                      controller.passwordSigninController!.text,
+                      controller.signInKey!),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  onTap: () {
+                    Get.to(
+                      () => const ForgotPassword(),
+                      transition: Transition.zoom,
+                    );
+                  },
+                  child: const Text(
+                    "Forgot Password",
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      color: Colors.brown,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const CustomText2(
+                        text: "Don't have an acount ? ",
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                        textAlign: TextAlign.center),
+                    InkWell(
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            color: Colors.brown,
+                            decoration: TextDecoration.underline),
+                      ),
+                      onTap: () {
+                        Get.to(() => const SignUp(),
+                            transition: Transition.zoom,
+                            duration: const Duration(milliseconds: 300));
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
