7b0dce925fc2e4b6ead7feacb63b80c77b88c11d
3li-3bdullah <PROGRMMER7@gmail.com>
2022-10-06 13:47:30 +0200

    Added new injection that's aboutAppViewmodel

--- sudaphone_sd/lib/shared/my_binding/my_binding.dart
+++ sudaphone_sd/lib/shared/my_binding/my_binding.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:sudaphone_sd/view_model/about_app_view_model.dart';
 import 'package:sudaphone_sd/view_model/categories_view_model.dart';
 import 'package:sudaphone_sd/view_model/details_view_model.dart';
 import 'package:sudaphone_sd/view_model/download_images_view_model.dart';
@@ -23,6 +24,7 @@ class MyBinding implements Bindings {
     Get.lazyPut(() => DetailsViewModel(), fenix: true);
     Get.lazyPut(() => CategoriesViewModel(), fenix: true);
     Get.lazyPut(() => FeedbackViewModel(), fenix: true);
+    Get.lazyPut(() => AboutAppViewModel(), fenix: true);
     Get.put(ThemesViewModel());
     Get.lazyPut(() => PublicData() , fenix: true);
   }
