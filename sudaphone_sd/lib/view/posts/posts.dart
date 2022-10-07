a4dc1c0a0d86eebe2250f76d74c523c393b32ac8
3li-3bdullah <PROGRMMER7@gmail.com>
2022-10-07 10:22:54 +0200

    First and foremost i have removed the futurebuider because i became using model so i used ListView.builder and wrapped it by Refresh indicator

--- sudaphone_sd/lib/view/posts/posts.dart
+++ sudaphone_sd/lib/view/posts/posts.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:lottie/lottie.dart';
-import 'package:sudaphone_sd/shared/components/custom_text2.dart';
 import 'package:sudaphone_sd/shared/components/custom_title.dart';
 import 'package:sudaphone_sd/view/people_have_liked/people_have_liked.dart';
 import 'package:sudaphone_sd/view/posts/components/comment_button.dart';
@@ -34,150 +32,160 @@ class Posts extends GetWidget<PostsViewModel> {
           color: Colors.white,
         ),
       ),
-      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: controller.postsCollections
-            .orderBy("dateTime", descending: true)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              shrinkWrap: true,
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
-                  return Card(
-                    child: Column(
-                      children: [
-                        ListTile(
-                          leading: CircleAvatar(
-                            backgroundImage: NetworkImage(
-                                "${snapshot.data!.docs[index].data()['profileUrl']}"),
+      body: RefreshIndicator(
+                onRefresh: () => controller.retrievePostsData(),
+                color: Colors.white,
+                backgroundColor: Colors.brown,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: controller.listOfPostModel.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Column(
+                        children: [
+                          ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                  "${controller.listOfPostModel[index].profileUrl}"),
+                            ),
+                            title: PostTitle(
+                              dateTime:
+                                  controller.listOfPostModel[index].dateTime,
+                              userName:
+                                  controller.listOfPostModel[index].userName,
+                            ),
+                            trailing: CustomPopupMenu(
+                                ownerUid:
+                                    controller.listOfPostModel[index].ownerUid,
+                                currentDocUid: controller
+                                    .listOfPostModel[index].currentDocUid),
+                            subtitle: PostSubtitle(
+                              imageUrl:
+                                  controller.listOfPostModel[index].imageUrl,
+                              isThereImageUrl: controller
+                                  .listOfPostModel[index].isThereImageUrl,
+                              text: controller.listOfPostModel[index].text,
+                            ),
                           ),
-                          title:
-                              PostTitle(currentDoc: snapshot.data!.docs[index]),
-                          trailing: CustomPopupMenu(
-                              ownerUid:
-                                  snapshot.data!.docs[index].data()['ownerUid'],
-                              currentDocUid: snapshot.data!.docs[index].id),
-                          subtitle: PostSubtitle(
-                              currentDoc: snapshot.data!.docs[index]),
-                        ),
-                        Divider(color: Colors.grey.withOpacity(0.2)),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  IconButton(
-                                    onPressed: () {
-                                      controller.handlePostLikes(
-                                          currentPostDocData:
-                                              snapshot.data!.docs[index]);
-                                    },
-                                    icon: Image.asset(
-                                      "assets/images/like.png",
-                                      color: snapshot.data!.docs[index]
-                                                      .data()['usersLiked']
-                                                  [controller.uid] !=
-                                              null
-                                          ? snapshot.data!.docs[index]
-                                                      .data()['usersLiked']
-                                                  [controller.uid]
-                                              ? Colors.pink
-                                              : Colors.brown
-                                          : Colors.brown,
-                                    ),
-                                  ),
-                                  const SizedBox(width: 20),
-                                  InkWell(
-                                    child: AnimatedSwitcher(
-                                      duration:
-                                          const Duration(milliseconds: 500),
-                                      transitionBuilder: (Widget child,
-                                          Animation<double> animation) {
-                                        return ScaleTransition(
-                                          scale: animation,
-                                          child: child,
-                                        );
-                                      },
-                                      child: Text(
-                                        snapshot.data!.docs[index]
-                                            .data()['likesCount']
-                                            .toString(),
-                                        key: ValueKey(
-                                          snapshot.data!.docs[index]
-                                              .data()['likesCount']
-                                              .toString(),
-                                        ),
-                                        style: TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.normal,
-                                          color: snapshot.data!.docs[index]
-                                                          .data()['usersLiked']
-                                                      [controller.uid] !=
-                                                  null
-                                              ? snapshot.data!.docs[index]
-                                                          .data()['usersLiked']
-                                                      [controller.uid]
-                                                  ? Colors.pink
-                                                  : Colors.grey.shade600
-                                              : Colors.grey.shade600,
+                          Divider(color: Colors.grey.withOpacity(0.2)),
+                          StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                              stream: controller.postsCollections
+                                  .doc(controller
+                                      .listOfPostModel[index].currentDocUid)
+                                  .snapshots(),
+                              builder: ((context, snapshot) {
+                                if (snapshot.hasData) {
+                                  return Row(
+                                    children: [
+                                      Expanded(
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            IconButton(
+                                              onPressed: () {
+                                                controller.handlePostLikes(
+                                                    currentDocUid:
+                                                        snapshot.data!.id,
+                                                    likesCount: snapshot
+                                                        .data!['likesCount']);
+                                              },
+                                              icon: Image.asset(
+                                                "assets/images/like.png",
+                                                color: snapshot.data![
+                                                                'usersLiked']
+                                                            [controller.uid] !=
+                                                        null
+                                                    ? snapshot.data![
+                                                                    'usersLiked']
+                                                                [controller
+                                                                    .uid] ==
+                                                            true
+                                                        ? Colors.pink
+                                                        : Colors.brown
+                                                    : Colors.brown,
+                                              ),
+                                            ),
+                                            const SizedBox(width: 20),
+                                            InkWell(
+                                              child: AnimatedSwitcher(
+                                                duration: const Duration(
+                                                    milliseconds: 500),
+                                                transitionBuilder:
+                                                    (Widget child,
+                                                        Animation<double>
+                                                            animation) {
+                                                  return ScaleTransition(
+                                                    scale: animation,
+                                                    child: child,
+                                                  );
+                                                },
+                                                child: Text(
+                                                  snapshot.data!['likesCount']
+                                                      .toString(),
+                                                  key: ValueKey(
+                                                    snapshot.data!['likesCount']
+                                                        .toString(),
+                                                  ),
+                                                  style: TextStyle(
+                                                    fontSize: 15,
+                                                    fontWeight:
+                                                        FontWeight.normal,
+                                                    color: snapshot.data![
+                                                                    'usersLiked']
+                                                                [controller
+                                                                    .uid] !=
+                                                            null
+                                                        ? snapshot.data![
+                                                                        'usersLiked']
+                                                                    [controller
+                                                                        .uid] ==
+                                                                true
+                                                            ? Colors.pink
+                                                            : Colors
+                                                                .grey.shade600
+                                                        : Colors.grey.shade600,
+                                                  ),
+                                                ),
+                                              ),
+                                              onTap: () {
+                                                Get.to(
+                                                    () => PeopleHaveLiked(
+                                                          peopleWhoLiked: snapshot
+                                                              .data![
+                                                                  'usersLiked']
+                                                              .keys
+                                                              .toList(),
+                                                          currentDoc:
+                                                              snapshot.data![
+                                                                  'usersLiked'],
+                                                        ),
+                                                    transition:
+                                                        Transition.zoom);
+                                              },
+                                            )
+                                          ],
                                         ),
                                       ),
-                                    ),
-                                    onTap: () {
-                                      Get.to(
-                                          () => PeopleHaveLiked(
-                                                peopleWhoLiked: snapshot
-                                                    .data!.docs[index]
-                                                    .data()['usersLiked']
-                                                    .keys
-                                                    .toList(),
-                                                currentDoc: snapshot
-                                                    .data!.docs[index]
-                                                    .data()['usersLiked'],
-                                              ),
-                                          transition: Transition.zoom);
-                                    },
-                                  )
-                                ],
-                              ),
-                            ),
-                            CommentButton(
-                                currentDoc: snapshot.data!.docs[index]),
-                          ],
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.only(top: 5),
-                        ),
-                      ],
-                    ),
-                  );
-                } else {
-                  return Container(
-                      height: MediaQuery.of(context).size.height,
-                      width: MediaQuery.of(context).size.width,
-                      alignment: Alignment.center,
-                      child: Image.asset('assets/images/loader.gif'));
-                }
-              },
-            );
-          } else if (!snapshot.hasData) {
-            return Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                alignment: Alignment.center,
-                child: Lottie.asset('assets/lotties/no_data.json'));
-          } else {
-            return Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                alignment: Alignment.center,
-                child: Image.asset('assets/images/loader.gif'));
-          }
-        },
-      ),
+                                      CommentButton(
+                                        currentDoc: snapshot.data!.id,
+                                        userName: snapshot.data!['userName'],
+                                      ),
+                                    ],
+                                  );
+                                } else {
+                                  return const SizedBox();
+                                }
+                              })),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 5),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
     );
   }
 }
