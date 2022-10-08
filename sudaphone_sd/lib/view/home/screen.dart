8f43e23d2966e594e108c1c7e9d3a558a7830f93
3li-3bdullah <PROGRMMER7@gmail.com>
2022-10-08 15:03:23 +0200

    Replaced the much widgets by custom classes that i created before like CustomLeading & SearchIcon etc..

--- sudaphone_sd/lib/view/home/screen.dart
+++ sudaphone_sd/lib/view/home/screen.dart
@@ -1,51 +1,26 @@
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:elastic_drawer/elastic_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
-import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
-import 'package:sudaphone_sd/shared/components/custom_text.dart';
-import 'package:sudaphone_sd/shared/components/custom_text2.dart';
-import 'package:sudaphone_sd/shared/constants.dart';
 import 'package:sudaphone_sd/view/categories/categories.dart';
-import 'package:sudaphone_sd/view/categories/phones/huawei.dart';
-import 'package:sudaphone_sd/view/categories/phones/iphone.dart';
-import 'package:sudaphone_sd/view/categories/phones/lenovo.dart';
-import 'package:sudaphone_sd/view/categories/phones/nokia.dart';
-import 'package:sudaphone_sd/view/categories/phones/oppo.dart';
-import 'package:sudaphone_sd/view/categories/phones/realme.dart';
-import 'package:sudaphone_sd/view/categories/phones/samsung.dart';
-import 'package:sudaphone_sd/view/categories/phones/tecno.dart';
-import 'package:sudaphone_sd/view/categories/phones/vivo.dart';
-import 'package:sudaphone_sd/view/categories/phones/xiaomi.dart';
-import 'package:sudaphone_sd/view/details/last_product_details.dart';
-import 'package:sudaphone_sd/view/home/components/best_gaming_carousel.dart';
 import 'package:sudaphone_sd/view/home/best_gaming_phones.dart';
+import 'package:sudaphone_sd/view/home/components/best_gaming_carousel.dart';
 import 'package:sudaphone_sd/view/home/components/categories_advertising.dart';
-import 'package:sudaphone_sd/view/home/components/build_images_carousel.dart';
-import 'package:sudaphone_sd/view/home/components/build_indicator_carousel.dart';
 import 'package:sudaphone_sd/view/home/components/categories_title.dart';
 import 'package:sudaphone_sd/view/home/components/custom_leading.dart';
-import 'package:sudaphone_sd/view/home/components/custom_listtile.dart';
 import 'package:sudaphone_sd/view/home/components/last_products_carousel.dart';
 import 'package:sudaphone_sd/view/home/components/main_carousel_slider.dart';
-import 'package:sudaphone_sd/view/home/components/screen_app_bar.dart';
-import 'package:sudaphone_sd/view/home/data_search.dart';
-import 'package:sudaphone_sd/view/home/favorite.dart';
 import 'package:sudaphone_sd/view/home/components/popular_carousel.dart';
-import 'package:sudaphone_sd/view_model/mydrawer_view_model.dart';
+import 'package:sudaphone_sd/view/home/components/screen_app_bar.dart';
+import 'package:sudaphone_sd/view/home/components/search_icon.dart';
+import 'package:sudaphone_sd/view/home/favorite.dart';
 import 'package:sudaphone_sd/view_model/public_data.dart';
 import 'package:sudaphone_sd/view_model/screen_view_model.dart';
-import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
 class Screen extends GetWidget<ScreenViewModel> {
   Screen({Key? key}) : super(key: key);
   final PublicData public_data = Get.find<PublicData>();
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    double halfheight = size.height / 2;
     return ElasticDrawer(
       mainColor: Colors.transparent,
       drawerColor: Colors.transparent,
@@ -53,29 +28,13 @@ class Screen extends GetWidget<ScreenViewModel> {
         body: NestedScrollView(
           floatHeaderSlivers: true,
           headerSliverBuilder: (context, innerBoxIsScrolled) => [
-            SliverAppBar(
-              title: const ScreenAppBar(),
+            const SliverAppBar(
+              title: ScreenAppBar(),
               elevation: 0,
               actions: [
-                GetBuilder<ThemesViewModel>(
-                  builder: (control) => Padding(
-                    padding: const EdgeInsets.only(right: 18.0),
-                    child: IconButton(
-                      icon: Image(
-                        image: const AssetImage("assets/images/search1.png"),
-                        height: 45,
-                        width: 45,
-                        color: control.theme == ThemeMode.dark
-                            ? Colors.white
-                            : Colors.black,
-                      ),
-                      onPressed: () =>
-                          showSearch(context: context, delegate: DataSearch()),
-                    ),
-                  ),
-                ),
+                SearchIcon()
               ],
-              leading: const CustomLeading(),
+              leading: CustomLeading(),
               floating: true,
             ),
           ],
