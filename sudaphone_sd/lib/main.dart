1368bf344ef0dcbfab018cb7c80a0002f00bbdbd
3li-3bdullah <PROGRMMER7@gmail.com>
2022-10-08 14:46:07 +0200

    I've removed the phoenix package and warpped the home widget animatedSplashScreen

--- sudaphone_sd/lib/main.dart
+++ sudaphone_sd/lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sudaphone_sd/shared/my_binding/my_binding.dart';
 import 'package:sudaphone_sd/view/my_drawer/mydrawer.dart';
 import 'package:sudaphone_sd/view/login/signin.dart';
+import 'package:sudaphone_sd/view/splash_screen/splash_screen.dart';
 import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
 import 'shared/constants.dart';
@@ -14,28 +15,28 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await GetStorage.init();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String? email = prefs.getString('email');
-  runApp( SudaphoneSD(email: email));
+  runApp(const SudaphoneSD());
 }
 
 class SudaphoneSD extends StatelessWidget {
-  const SudaphoneSD({this.email ,Key? key}) : super(key: key);
-  final String? email;
+  const SudaphoneSD({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return GetBuilder<ThemesViewModel>(
       init: ThemesViewModel(),
-      builder:(control)=> GetMaterialApp(
-          debugShowCheckedModeBanner: false,
-          themeMode: control.theme,
-          theme: Themes.lightTheme,
-          darkTheme: Themes.darkTheme,
-          title: "SudaPhone SD",
-          initialBinding: MyBinding(),
-          home: email != null
-                ?  const MyDrawer()
-                : const SignIn()),
+      builder: (control) => GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode: control.theme,
+        theme: Themes.lightTheme,
+        darkTheme: Themes.darkTheme,
+        title: "SudaPhone SD",
+        initialBinding: MyBinding(),
+        home: AnimatedSplashScreen(
+            splash: SplashScreen(),
+            nextScreen: control.email != null ? const MyDrawer() : const SignIn(),
+            splashIconSize: Get.height,
+            ) 
+      ),
     );
   }
 }
