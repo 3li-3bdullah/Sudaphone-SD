6f8733d84ef8b40e0389f0fda6a935caa9602804
3li-3bdullah <PROGRMMER7@gmail.com>
2022-10-08 15:08:23 +0200

    Warpped the padding with card to add shadow to the shape ;}

--- sudaphone_sd/lib/view/home/components/popular_carousel.dart
+++ sudaphone_sd/lib/view/home/components/popular_carousel.dart
@@ -10,98 +10,103 @@ class PopularCarousel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
-                  height: MediaQuery.of(context).size.height / 3,
-                  width: MediaQuery.of(context).size.width,
-                  child: FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                    future: FirebaseFirestore.instance
-                        .collection("carousel")
-                        .doc("popular")
-                        .collection("carousel")
-                        .get(),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return CarouselSlider.builder(
-                          itemCount: snapshot.data!.docs.length,
-                          itemBuilder: (context, index, __) {
-                            return SizedBox(
-                              width: Get.width,
-                              height: Get.height / 4,
-                              child: Column(children: [
-                                Expanded(
-                                  child: ClipRRect(
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(10)),
-                                    child: FadeInImage.assetNetwork(
-                                      placeholder: 'assets/images/loader.gif',
-                                      image: snapshot.data?.docs[index]
-                                          .data()['imageUrl'],
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                                Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: CustomText(
-                                    text:
-                                        "${snapshot.data!.docs[index].data()['name']}",
-                                    fontWeight: FontWeight.normal,
-                                    textAlign: TextAlign.end,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                              ]),
-                            );
-                          },
-                          options: CarouselOptions(
-                            initialPage: 0,
-                            autoPlay: true,
-                            scrollDirection: Axis.horizontal,
-                            enableInfiniteScroll: true,
-                            enlargeCenterPage: true,
-                            aspectRatio: 16 / 9,
-                            viewportFraction: 0.8,
-                            autoPlayCurve: Curves.fastOutSlowIn,
-                            autoPlayAnimationDuration:
-                                const Duration(milliseconds: 200),
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 3,
+      width: MediaQuery.of(context).size.width,
+      child: FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        future: FirebaseFirestore.instance
+            .collection("carousel")
+            .doc("popular")
+            .collection("carousel")
+            .get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return CarouselSlider.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index, __) {
+                return Card(
+                  elevation: 8,
+                  clipBehavior: Clip.antiAlias,
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: SizedBox(
+                      width: Get.width,
+                      height: Get.height / 4,
+                      child: Column(children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
+                            child: FadeInImage.assetNetwork(
+                              placeholder: 'assets/images/loader.gif',
+                              image:
+                                  snapshot.data?.docs[index].data()['imageUrl'],
+                              fit: BoxFit.cover,width: Get.width * 0.9,
+                            ),
                           ),
-                        );
-                      } else if (snapshot.connectionState ==
-                          ConnectionState.waiting) {
-                        return Center(
-                          child: Image.asset("assets/images/loader.gif"),
-                        );
-                      } else if (snapshot.hasError) {
-                        return Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            CustomText(
-                              text: "Oops!",
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              textAlign: TextAlign.center,
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            CustomText(
-                              text: snapshot.error.toString(),
-                              fontSize: 15,
-                              fontWeight: FontWeight.normal,
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        );
-                      } else {
-                        return const Center(
-                          child: CircularProgressIndicator(
-                            color: Colors.red,
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: CustomText(
+                            text:
+                                "${snapshot.data!.docs[index].data()['name']}",
+                            fontWeight: FontWeight.normal,
+                            textAlign: TextAlign.end,
+                            fontSize: 16,
                           ),
-                        );
-                      }
-                    },
+                        ),
+                      ]),
+                    ),
                   ),
                 );
+              },
+              options: CarouselOptions(
+                initialPage: 0,
+                autoPlay: true,
+                scrollDirection: Axis.horizontal,
+                enableInfiniteScroll: true,
+                enlargeCenterPage: true,
+                aspectRatio: 16 / 9,
+                viewportFraction: 0.8,
+                autoPlayCurve: Curves.fastOutSlowIn,
+                autoPlayAnimationDuration: const Duration(milliseconds: 150),
+              ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: Image.asset("assets/images/loader.gif"),
+            );
+          } else if (snapshot.hasError) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CustomText(
+                  text: "Oops!",
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomText(
+                  text: snapshot.error.toString(),
+                  fontSize: 15,
+                  fontWeight: FontWeight.normal,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.red,
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
