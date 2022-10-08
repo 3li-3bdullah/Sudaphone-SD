f8003503a13aa74208ae13775766bd6ac001c8f3
3li-3bdullah <PROGRMMER7@gmail.com>
2022-10-08 14:45:00 +0200

    I've created new file that has the splash screen widgets

---
+++ sudaphone_sd/lib/view/splash_screen/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        SizedBox(
+          height: Get.height,
+          width: Get.width,
+        ),
+        Positioned(
+          height: Get.height,
+          width: Get.width,
+          child: Center(
+            child: Image.asset(
+              'assets/images/logo/logo.jpg',
+              fit: BoxFit.cover,
+              height: Get.height,
+              width: Get.width,
+            ),
+          ),
+        ),
+        Positioned(
+            child: Container(
+          color: Colors.white30,
+          height: Get.height,
+          width: Get.width,
+        )),
+        Positioned(child: Image.asset('assets/images/logo/app_logo_trans.png'))
+      ],
+    );
+  }
+}
