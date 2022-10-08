fb9f71ccb16ff781f7cc6da3e2dceb4adb3a0dd7
3li-3bdullah <PROGRMMER7@gmail.com>
2022-10-08 14:57:39 +0200

    I've used vars from model instead of that come from StreamBuilder method

--- sudaphone_sd/lib/view/posts/components/post_title.dart
+++ sudaphone_sd/lib/view/posts/components/post_title.dart
@@ -7,8 +7,10 @@ import 'package:sudaphone_sd/shared/components/custom_text2.dart';
 import 'package:sudaphone_sd/view_model/posts_view_model.dart';
 
 class PostTitle extends GetWidget<PostsViewModel> {
-  const PostTitle({Key? key, required this.currentDoc}) : super(key: key);
-  final QueryDocumentSnapshot<Map<String, dynamic>> currentDoc;
+  const PostTitle({Key? key, required this.dateTime, required this.userName})
+      : super(key: key);
+  final String userName;
+  final dynamic dateTime;
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +20,14 @@ class PostTitle extends GetWidget<PostsViewModel> {
         Container(
           margin: const EdgeInsets.only(top: 10),
           child: CustomText(
-            text: "${currentDoc.data()['userName']}",
+            text: "$userName",
             fontSize: 17,
             fontWeight: FontWeight.bold,
             textAlign: TextAlign.right,
           ),
         ),
         CustomText2(
-          text: controller.handleDate(currentDoc.data()['dateTime']),
+          text: controller.handleDate(dateTime),
           fontSize: 13,
           fontWeight: FontWeight.normal,
           color: Colors.grey,
