6f6703cd15424187c51d3a39038bc05a59c00cd0
3li-3bdullah <PROGRMMER7@gmail.com>
2022-12-13 09:59:25 +0200

    Added some features

--- sudaphone_sd/lib/view/settings/components/custom_row.dart
+++ sudaphone_sd/lib/view/settings/components/custom_row.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone_sd/shared/components/custom_text.dart';
 
