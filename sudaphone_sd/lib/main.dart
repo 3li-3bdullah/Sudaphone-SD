1f86b799f874f177cafc8b28e2dd68d5071350dd
3li-3bdullah <PROGRMMER7@gmail.com>
2023-01-11 01:36:58 +0200

    Added GetX to check the internet status

--- sudaphone_sd/lib/main.dart
+++ sudaphone_sd/lib/main.dart
@@ -6,7 +6,9 @@ import 'package:get_storage/get_storage.dart';
 import 'package:sudaphone_sd/shared/my_binding/my_binding.dart';
 import 'package:sudaphone_sd/view/my_drawer/mydrawer.dart';
 import 'package:sudaphone_sd/view/login/signin.dart';
+import 'package:sudaphone_sd/view/no_internet_connection/no_internet_connection.dart';
 import 'package:sudaphone_sd/view/splash_screen/splash_screen.dart';
+import 'package:sudaphone_sd/view_model/splash_screen_view_model.dart';
 import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
 import 'shared/constants.dart';
@@ -25,18 +27,22 @@ class SudaphoneSD extends StatelessWidget {
     return GetBuilder<ThemesViewModel>(
       init: ThemesViewModel(),
       builder: (control) => GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        themeMode: control.theme,
-        theme: Themes.lightTheme,
-        darkTheme: Themes.darkTheme,
-        title: "SudaPhone SD",
-        initialBinding: MyBinding(),
-        home: AnimatedSplashScreen(
-            splash: SplashScreen(),
-            nextScreen: control.email != null ? const MyDrawer() : const SignIn(),
-            splashIconSize: Get.height,
-            ) 
-      ),
+          debugShowCheckedModeBanner: false,
+          themeMode: control.theme,
+          theme: Themes.lightTheme,
+          darkTheme: Themes.darkTheme,
+          title: "SudaPhone SD",
+          initialBinding: MyBinding(),
+          home: GetX<SplashScreenViewModel>(
+            builder: (controller) {
+              return AnimatedSplashScreen(
+                splash: SplashScreen(),
+                nextScreen:
+                  controller.internetConnectionChecker.value == 1 ?  control.email != null ? const MyDrawer() : const SignIn() : NoInternetConnection(),
+                splashIconSize: Get.height,
+              );
+            },
+          )),
     );
   }
 }
