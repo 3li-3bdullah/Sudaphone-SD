2ec21171b691596d4dba9d7820cc758e99f47804
3li-3bdullah <PROGRMMER7@gmail.com>
2023-01-11 01:25:06 +0200

    I have declared some vars and methods to check the internet status

--- sudaphone_sd/lib/view_model/splash_screen_view_model.dart
+++ sudaphone_sd/lib/view_model/splash_screen_view_model.dart
@@ -1,11 +1,30 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class SplashScreenViewModel extends GetxController {
+  //* ---------------------- Declaring Variables --------------
 
+  late StreamSubscription<InternetConnectionStatus> listener;
+  RxInt internetConnectionChecker = 0.obs;
+
+  //* ---------------------- Declaring Methods --------------
 
   @override
   void onInit() {
     super.onInit();
-    
+    listener = InternetConnectionChecker()
+        .onStatusChange
+        .listen((InternetConnectionStatus status) {
+      switch (status) {
+        case InternetConnectionStatus.connected:
+          internetConnectionChecker.value = 1;
+          break;
+        case InternetConnectionStatus.disconnected:
+          internetConnectionChecker.value = 0;
+          break;
+      }
+    });
   }
 }
