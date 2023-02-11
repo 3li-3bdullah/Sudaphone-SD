3951710c794bdbafdad9c7b1423f4828ab3cb986
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-11 10:25:36 +0200

    I have removed un used import

--- sudaphone_sd/lib/view_model/mydrawer_view_model.dart
+++ sudaphone_sd/lib/view_model/mydrawer_view_model.dart
@@ -1,10 +1,7 @@
-import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sudaphone_sd/view/login/signin.dart';
 
