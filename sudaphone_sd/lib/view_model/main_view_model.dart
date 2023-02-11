3ae32c03bfcf0fab8638e23e765a040a0f5bf74f
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-11 10:26:30 +0200

    I have refactored the code

--- sudaphone_sd/lib/view_model/main_view_model.dart
+++ sudaphone_sd/lib/view_model/main_view_model.dart
@@ -2,27 +2,26 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
-class MainViewModel extends GetxController{
-Future<FirebaseApp> fbApp = Firebase.initializeApp();
-FirebaseAuth auth = FirebaseAuth.instance;
-RxBool isLogin = false.obs;
+class MainViewModel extends GetxController {
+  Future<FirebaseApp> fbApp = Firebase.initializeApp();
+  FirebaseAuth auth = FirebaseAuth.instance;
+  RxBool isLogin = false.obs;
 
-
-///Declaring Methods
+  ///Declaring Methods
   @override
   void onInit() {
     super.onInit();
     refreshFirebaseUser();
   }
-Future refreshFirebaseUser() async {
-  if (auth.currentUser != null) {
-    isLogin.value = true;
-    final user = auth.currentUser;
-    final idTokenResult = await user!.getIdTokenResult(true);
-    return idTokenResult;
-  } else{
-    isLogin.value =false;
-  }
-}
 
+  Future refreshFirebaseUser() async {
+    if (auth.currentUser != null) {
+      isLogin.value = true;
+      final user = auth.currentUser;
+      final idTokenResult = await user!.getIdTokenResult(true);
+      return idTokenResult;
+    } else {
+      isLogin.value = false;
+    }
+  }
 }
