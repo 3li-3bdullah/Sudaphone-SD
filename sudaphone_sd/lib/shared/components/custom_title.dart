bb398eea852a56a92c4d1ce5ddb6f45d63bdf461
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-11 15:36:28 +0200

    Added some layout ;)

--- sudaphone_sd/lib/shared/components/custom_title.dart
+++ sudaphone_sd/lib/shared/components/custom_title.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone_sd/shared/components/custom_text.dart';
 import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
 class CustomTitle extends StatelessWidget {
   const CustomTitle(
-      {Key? key, required this.text, required this.underLineWidget, this.showUnderLine = true})
+      {Key? key,
+      required this.text,
+      required this.underLineWidget,
+      this.showUnderLine = true})
       : super(key: key);
   final String text;
   final double underLineWidget;
@@ -21,23 +22,25 @@ class CustomTitle extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 8.0),
           child: CustomText(
             text: text,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
             textAlign: TextAlign.center,
           ),
         ),
-       showUnderLine ? GetBuilder<ThemesViewModel>(
-          builder: (control) => Container(
-            height: 4,
-            width: underLineWidget,
-            alignment: Alignment.centerLeft,
-            decoration: BoxDecoration(
-                color: control.theme == ThemeMode.dark
-                    ? Colors.grey
-                    : Colors.brown,
-                borderRadius: const BorderRadius.all(Radius.circular(5))),
-          ),
-        ) : const SizedBox()
+        showUnderLine
+            ? GetBuilder<ThemesViewModel>(
+                builder: (control) => Container(
+                  height: 2.5,
+                  width: underLineWidget,
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                      color: control.theme == ThemeMode.dark
+                          ? Colors.grey
+                          : Colors.brown,
+                      borderRadius: const BorderRadius.all(Radius.circular(5))),
+                ),
+              )
+            : const SizedBox()
       ],
     );
   }
