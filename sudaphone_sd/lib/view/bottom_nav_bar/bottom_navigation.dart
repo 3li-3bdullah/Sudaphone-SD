b2dbba48a97b6dccc3fd1097bd6ae072e97d193d
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-17 23:22:22 +0200

    I have removed the list of Widget to ScreenController

--- sudaphone_sd/lib/view/bottom_nav_bar/bottom_navigation.dart
+++ sudaphone_sd/lib/view/bottom_nav_bar/bottom_navigation.dart
@@ -1,35 +1,24 @@
-
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone_sd/shared/constants.dart';
-import 'package:sudaphone_sd/view/categories/categories.dart';
-import 'package:sudaphone_sd/view/posts/posts.dart';
-import 'package:sudaphone_sd/view/home/screen.dart';
-import 'package:sudaphone_sd/view/settings/settings.dart';
 import 'package:sudaphone_sd/view_model/screen_view_model.dart';
-import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:sudaphone_sd/view_model/themes_view_model.dart';
-
 
 // ignore: must_be_immutable
 class BottomNavigation extends GetWidget<ScreenViewModel> {
   BottomNavigation({Key? key}) : super(key: key);
-  final List<Widget> screens = [
-      Screen(),
-    const Categories(),
-     const Posts(),
-    Setting(),
-  ];
   @override
   Widget build(BuildContext context) {
     return GetBuilder<ScreenViewModel>(
       init: ScreenViewModel(),
       builder: (controller) => Scaffold(
         bottomNavigationBar: GetBuilder<ThemesViewModel>(
-          builder:(control) => BottomNavyBar(
+          builder: (control) => BottomNavyBar(
             selectedIndex: controller.activeIndex,
             showElevation: true,
-            backgroundColor: control.theme == ThemeMode.dark ? kDark1 : Colors.white,
+            backgroundColor:
+                control.theme == ThemeMode.dark ? kDark1 : Colors.white,
             onItemSelected: (index) {
               controller.onSelectedItem(index);
             },
@@ -73,7 +62,7 @@ class BottomNavigation extends GetWidget<ScreenViewModel> {
             ],
           ),
         ),
-        body: screens[controller.activeIndex],
+        body: controller.screens[controller.activeIndex],
       ),
     );
   }
