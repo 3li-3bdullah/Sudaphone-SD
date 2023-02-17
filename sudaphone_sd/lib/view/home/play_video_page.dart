6889d580df68f20de07939429390760eaf9d72f0
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-17 23:32:21 +0200

    Renamed the class to be readable

--- sudaphone_sd/lib/view/home/play_video_page.dart
+++ sudaphone_sd/lib/view/home/play_video_page.dart
@@ -1,9 +1,9 @@
 import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
-import 'package:sudaphone_sd/view_model/screen_view_model.dart';
+import 'package:sudaphone_sd/view_model/home_view_model.dart';
 import 'package:get/get.dart';
 
-class PlayVideoPage extends GetWidget<ScreenViewModel> {
+class PlayVideoPage extends GetWidget<HomeViewModel> {
   const PlayVideoPage({Key? key, required this.videoUrl}) : super(key: key);
   final String videoUrl;
   @override
