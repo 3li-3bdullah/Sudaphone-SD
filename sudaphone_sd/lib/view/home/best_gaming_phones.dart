34adff040f6ee260a635df066219f2e0da76173f
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-19 23:07:43 +0200

    I have removed second shadow

--- sudaphone_sd/lib/view/home/best_gaming_phones.dart
+++ sudaphone_sd/lib/view/home/best_gaming_phones.dart
@@ -19,54 +19,53 @@ class BestGamingPhones extends GetWidget<HomeViewModel> {
                 margin:
                     const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
                 child: Container(
-                    decoration: const BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                            color:Colors.black38,
-                            blurRadius: 15.0,
-                            offset: Offset(12, 12)),
-                        BoxShadow(
-                            color:Colors.transparent,
-                            blurRadius: 15.0,
-                            offset: Offset(-12, -12)),
-                      ],
-                    ),
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.all(Radius.circular(10)),
-                      child: InkWell(
-                        onTap: () {
-                          Get.to(() => LastProducDetails(
-                              collection: "forGames",
-                              phoneDoc: controller.listBGPhones![index].docId));
-                        },
-                        child: GetBuilder<ThemesViewModel>(
-                          builder: (control) => Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              FadeInImage.assetNetwork(
-                                image: controller.listBGPhones![index].image,
-                                fit: BoxFit.cover,
-                                placeholder: 'assets/images/loader.gif',
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black38,
+                          blurRadius: 15.0,
+                          offset: Offset(12, 12)),
+                      // BoxShadow(
+                      //     color:Colors.transparent,
+                      //     blurRadius: 15.0,
+                      //     offset: Offset(-12, -12)),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    child: InkWell(
+                      onTap: () {
+                        Get.to(() => LastProducDetails(
+                            collection: "forGames",
+                            phoneDoc: controller.listBGPhones![index].docId));
+                      },
+                      child: GetBuilder<ThemesViewModel>(
+                        builder: (control) => Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            FadeInImage.assetNetwork(
+                              image: controller.listBGPhones![index].image,
+                              fit: BoxFit.cover,
+                              placeholder: 'assets/images/loader.gif',
+                            ),
+                            Positioned(
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 20),
+                                child: CustomText2(
+                                    text: controller.listBGPhones![index].name,
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.normal,
+                                    textAlign: TextAlign.center,
+                                    color: Colors.white),
                               ),
-                              Positioned(
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 20),
-                                  child: CustomText2(
-                                      text:
-                                          controller.listBGPhones![index].name,
-                                      fontSize: 17,
-                                      fontWeight: FontWeight.normal,
-                                      textAlign: TextAlign.center,
-                                      color: Colors.white),
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
                   ),
+                ),
               );
             }),
       ),
