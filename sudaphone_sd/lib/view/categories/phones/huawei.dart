edc8688d36f800671e8b1518930ab57a20f8f276
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-24 16:43:34 +0200

    Added custom layouts for appbar

--- sudaphone_sd/lib/view/categories/phones/huawei.dart
+++ sudaphone_sd/lib/view/categories/phones/huawei.dart
@@ -2,8 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone_sd/shared/components/custom_title.dart';
-import 'package:sudaphone_sd/shared/components/leading.dart';
 import 'package:sudaphone_sd/shared/components/phone_list.dart';
+import 'package:sudaphone_sd/view/home/components/custom_leading.dart';
 import 'package:sudaphone_sd/view_model/categories_view_model.dart';
 
 class Huawei extends GetWidget<CategoriesViewModel> {
@@ -14,8 +14,9 @@ class Huawei extends GetWidget<CategoriesViewModel> {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        title: const  CustomTitle(text: "Huawei", underLineWidget: 50),
-          leading:  const Leading(),
+        title: const  CustomTitle(text: "Huawei", underLineWidget: 50,showUnderLine: false),
+          leading:  const CustomLeading(),
+          centerTitle: true,
       ),
       body: FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
           future: controller.phonesCategory
