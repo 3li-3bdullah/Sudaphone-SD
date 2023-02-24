e476226fe22faf92fac392d346108fe066a1df6c
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-24 12:59:04 +0200

    Added getwidget to change color when user change the mode

--- sudaphone_sd/lib/view/about_app/components/custom_container.dart
+++ sudaphone_sd/lib/view/about_app/components/custom_container.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:sudaphone_sd/shared/components/custom_divider.dart';
+import 'package:sudaphone_sd/shared/constants.dart';
 import 'package:sudaphone_sd/view/details/components/custom_text_details.dart';
 
-class CustomContainer extends StatelessWidget {
+import '../../../view_model/themes_view_model.dart';
+
+class CustomContainer extends GetWidget<ThemesViewModel> {
   const CustomContainer(
       {Key? key,
       this.text1Size = 15,
@@ -27,85 +31,95 @@ class CustomContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 10),
-      margin: EdgeInsets.symmetric(horizontal: 10),
-      decoration: BoxDecoration(
-        color: Colors.brown[300],
-        borderRadius: BorderRadius.all(
-          Radius.circular(10),
-        ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 10.0, bottom: 10),
-            child: CustomText2(
-              text: header,
-              color: Colors.white,
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-              textAlign: TextAlign.left,
+    return GetBuilder<ThemesViewModel>(
+      builder: (controller) => Container(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+            color:
+                controller.theme == ThemeMode.dark ? kDark2 : Colors.brown[300],
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
             ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              InkWell(
-                onTap: () {
-                  ontap1();
-                },
-                child: Row(children: [
+            boxShadow: [
+              controller.theme == ThemeMode.dark
+                  ? BoxShadow(
+                      offset: Offset(5, 5),
+                      color: Colors.black26,
+                      blurRadius: 3)
+                  : BoxShadow(),
+            ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0, bottom: 10),
+              child: CustomText2(
+                text: header,
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                textAlign: TextAlign.left,
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  onTap: () {
+                    ontap1();
+                  },
+                  child: Row(children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: radius,
+                        backgroundImage: AssetImage(image1)),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    CustomText2(
+                      text: text1,
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      textAlign: TextAlign.center,
+                    )
+                  ]),
+                ),
+              ],
+            ),
+            CustomDivider(color: Colors.grey, horizontal: 20, vertical: 8),
+            InkWell(
+              onTap: () {
+                ontap2();
+              },
+              child: Row(
+                children: [
                   SizedBox(
                     width: 20,
                   ),
                   CircleAvatar(
+                      radius: 14,
                       backgroundColor: Colors.white,
-                      radius: radius,
-                      backgroundImage: AssetImage(image1)),
+                      backgroundImage: AssetImage(image2)),
                   SizedBox(
                     width: 20,
                   ),
                   CustomText2(
-                    text: text1,
+                    text: text2,
                     color: Colors.white,
-                    fontSize: 15,
+                    fontSize: 14,
                     fontWeight: FontWeight.w400,
                     textAlign: TextAlign.center,
                   )
-                ]),
+                ],
               ),
-            ],
-          ),
-          CustomDivider(color: Colors.grey, horizontal: 20, vertical: 8),
-          InkWell(
-            onTap: () {
-              ontap2();
-            },
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 20,
-                ),
-                CircleAvatar(
-                    radius: 14,
-                    backgroundColor: Colors.white,
-                    backgroundImage: AssetImage(image2)),
-                SizedBox(
-                  width: 20,
-                ),
-                CustomText2(
-                  text: text2,
-                  color: Colors.white,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  textAlign: TextAlign.center,
-                )
-              ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
