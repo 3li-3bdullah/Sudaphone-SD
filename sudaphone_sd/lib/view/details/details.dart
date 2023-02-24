36bafe77196419a4aeecd90c8d2700a772ec1c47
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-24 14:30:01 +0200

    Added errorbuilder

--- sudaphone_sd/lib/view/details/details.dart
+++ sudaphone_sd/lib/view/details/details.dart
@@ -65,6 +65,14 @@ class Details extends GetWidget<DetailsViewModel> {
                               placeholder: 'assets/images/loader.gif',
                               height:
                                   MediaQuery.of(context).size.height / 2 + 80,
+                              placeholderErrorBuilder:
+                                  ((context, error, stackTrace) => Image.asset(
+                                        "assets/images/shimmer3.jpg",
+                                      )),
+                              imageErrorBuilder:
+                                  ((context, error, stackTrace) => Image.asset(
+                                        "assets/images/shimmer3.jpg",
+                                      )),
                               width: double.infinity,
                               fit: BoxFit.contain,
                             ),
@@ -82,150 +90,164 @@ class Details extends GetWidget<DetailsViewModel> {
                 ),
                 panel: SingleChildScrollView(
                   child: ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        topLeft:Radius.circular(20),
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20),
                         topRight: Radius.circular(20)),
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            InkWell(
-                              child: Center(
-                                child: Container(
-                                  margin:
-                                      const EdgeInsets.only(top: 10, bottom: 10),
-                                  width: 50,
-                                  height: 8,
-                                  decoration: const BoxDecoration(
-                                      color: Colors.brown,
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(12))),
-                                ),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          InkWell(
+                            child: Center(
+                              child: Container(
+                                margin:
+                                    const EdgeInsets.only(top: 10, bottom: 10),
+                                width: 50,
+                                height: 8,
+                                decoration: const BoxDecoration(
+                                    color: Colors.brown,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(12))),
                               ),
-                              onTap: () {
-                                controller.togglePanel();
-                              },
                             ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            CustomText(
-                                text: "${_snapshot.data?['name']}",
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                textAlign: TextAlign.start),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                CustomText2(
-                                    text:
-                                        "Price : \$ ${_snapshot.data?['price']}",
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.pink,
-                                    textAlign: TextAlign.left),
-                                Row(
-                                  children: [
-                                    IconButton(
-                                      iconSize: 38,
-                                      icon: _snapshot.data!.data()!['usersLiked']
-                                                  [controller.uid] !=
-                                              null
-                                          ? _snapshot.data!.data()!['usersLiked']
-                                                      [controller.uid] ==
-                                                  true
-                                              ? Image.asset(
-                                                  "assets/images/like.png",
-                                                  color: Colors.pink,
-                                                )
-                                              : Image.asset(
-                                                  "assets/images/like.png",
-                                                  color: Colors.brown,
-                                                )
-                                          : Image.asset(
-                                              "assets/images/like.png",
-                                              color: Colors.brown,
-                                            ),
-                                      onPressed: () {
-                                        controller.handlePhoneLikes(
-                                            collection: collction,
-                                            docOne: docOne,
-                                            snapshot: _snapshot.data);
+                            onTap: () {
+                              controller.togglePanel();
+                            },
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          CustomText(
+                              text: "${_snapshot.data?['name']}",
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              textAlign: TextAlign.start),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              CustomText2(
+                                  text:
+                                      "Price : \$ ${_snapshot.data?['price']}",
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.pink,
+                                  textAlign: TextAlign.left),
+                              Row(
+                                children: [
+                                  IconButton(
+                                    iconSize: 38,
+                                    icon: _snapshot.data!.data()!['usersLiked']
+                                                [controller.uid] !=
+                                            null
+                                        ? _snapshot.data!.data()!['usersLiked']
+                                                    [controller.uid] ==
+                                                true
+                                            ? Image.asset(
+                                                "assets/images/like.png",
+                                                color: Colors.pink,
+                                              )
+                                            : Image.asset(
+                                                "assets/images/like.png",
+                                                color: Colors.brown,
+                                              )
+                                        : Image.asset(
+                                            "assets/images/like.png",
+                                            color: Colors.brown,
+                                          ),
+                                    onPressed: () {
+                                      controller.handlePhoneLikes(
+                                          collection: collction,
+                                          docOne: docOne,
+                                          snapshot: _snapshot.data);
+                                    },
+                                  ),
+                                  IconButton(
+                                    icon: AnimatedSwitcher(
+                                      duration:
+                                          const Duration(milliseconds: 400),
+                                      transitionBuilder: (Widget child,
+                                          Animation<double> animation) {
+                                        return ScaleTransition(
+                                          scale: animation,
+                                          child: child,
+                                        );
                                       },
+                                      child: Text(
+                                          "${_snapshot.data?['likesCount']}",
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                            color: _snapshot.data?['usersLiked']
+                                                        ['${controller.uid}'] ==
+                                                    true
+                                                ? Colors.pink
+                                                : Colors.grey,
+                                          ),
+                                          key: ValueKey(
+                                              "${_snapshot.data?['likesCount']}"),
+                                          textAlign: TextAlign.left),
                                     ),
-                                    IconButton(
-                                      icon: AnimatedSwitcher(
-                                        duration:
-                                            const Duration(milliseconds: 400),
-                                        transitionBuilder: (Widget child,
-                                            Animation<double> animation) {
-                                          return ScaleTransition(
-                                            scale: animation,
-                                            child: child,
-                                          );
-                                        },
-                                        child: Text(
-                                            "${_snapshot.data?['likesCount']}",
-                                            style: TextStyle(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.bold,
-                                              color: _snapshot.data?['usersLiked']
-                                                          ['${controller.uid}'] ==
-                                                      true
-                                                  ? Colors.pink
-                                                  : Colors.grey,
-                                            ),
-                                            key: ValueKey(
-                                                "${_snapshot.data?['likesCount']}"),
-                                            textAlign: TextAlign.left),
-                                      ),
-                                      onPressed: () {
-                                        Get.to(
-                                            () => PeopleHaveLiked(
-                                                peopleWhoLiked: _snapshot
-                                                    .data?['usersLiked'].keys
-                                                    .toList(),
-                                                currentDoc: _snapshot
-                                                    .data?['usersLiked']),
-                                            transition: Transition.zoom);
-                                      },
-                                    ),
-                                  ],
-                                ),
-                                SaveIcon(
-                                    snapshot: _snapshot,
-                                    collction: collction,
-                                    docOne: docOne,
-                                    docTwo: docTwo)
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 15,
-                            ),
-                            DetailsText(detail: "Ram", text: "${_snapshot.data?['ram']}"),
-                            Divider(color: Colors.black.withOpacity(0.3)),
-                            DetailsText(detail: "Storage", text: "${_snapshot.data?['storage']}"),
-                            Divider(color: Colors.black.withOpacity(0.3)),
-                            DetailsText(detail: "CPU", text: "${_snapshot.data?['cpu']}"),
-                            Divider(color: Colors.black.withOpacity(0.3)),
-                            DetailsText(detail: "Rear Camera", text: "${_snapshot.data?['rearCamera']}"),
-                            Divider(color: Colors.black.withOpacity(0.3)),
-                            DetailsText(detail: "Front Camera", text: "${_snapshot.data?['frontCamera']}"),
-                            Divider(color: Colors.black.withOpacity(0.3)),
-                            DetailsText(detail: "Battery", text: "${_snapshot.data?['battery']}"),
-                            Divider(color: Colors.black.withOpacity(0.3)),
-                            DetailsText(detail: "Display", text: "${_snapshot.data?['screen']}"),
-                            Divider(color: Colors.black.withOpacity(0.3)),
-                            DetailsText(detail: "Operating System", text: "${_snapshot.data?['os']}"),
-                            Divider(color: Colors.black.withOpacity(0.3)),
-                          ],
-                        ),
+                                    onPressed: () {
+                                      Get.to(
+                                          () => PeopleHaveLiked(
+                                              peopleWhoLiked: _snapshot
+                                                  .data?['usersLiked'].keys
+                                                  .toList(),
+                                              currentDoc: _snapshot
+                                                  .data?['usersLiked']),
+                                          transition: Transition.zoom);
+                                    },
+                                  ),
+                                ],
+                              ),
+                              SaveIcon(
+                                  snapshot: _snapshot,
+                                  collction: collction,
+                                  docOne: docOne,
+                                  docTwo: docTwo)
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          DetailsText(
+                              detail: "Ram", text: "${_snapshot.data?['ram']}"),
+                          Divider(color: Colors.black.withOpacity(0.3)),
+                          DetailsText(
+                              detail: "Storage",
+                              text: "${_snapshot.data?['storage']}"),
+                          Divider(color: Colors.black.withOpacity(0.3)),
+                          DetailsText(
+                              detail: "CPU", text: "${_snapshot.data?['cpu']}"),
+                          Divider(color: Colors.black.withOpacity(0.3)),
+                          DetailsText(
+                              detail: "Rear Camera",
+                              text: "${_snapshot.data?['rearCamera']}"),
+                          Divider(color: Colors.black.withOpacity(0.3)),
+                          DetailsText(
+                              detail: "Front Camera",
+                              text: "${_snapshot.data?['frontCamera']}"),
+                          Divider(color: Colors.black.withOpacity(0.3)),
+                          DetailsText(
+                              detail: "Battery",
+                              text: "${_snapshot.data?['battery']}"),
+                          Divider(color: Colors.black.withOpacity(0.3)),
+                          DetailsText(
+                              detail: "Display",
+                              text: "${_snapshot.data?['screen']}"),
+                          Divider(color: Colors.black.withOpacity(0.3)),
+                          DetailsText(
+                              detail: "Operating System",
+                              text: "${_snapshot.data?['os']}"),
+                          Divider(color: Colors.black.withOpacity(0.3)),
+                        ],
                       ),
                     ),
+                  ),
                 ),
               ),
             );
