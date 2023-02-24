82bae0edd74edc3740aa9fa859b13c0d126436fb
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-24 16:44:22 +0200

    Added custom layouts for appbar

--- sudaphone_sd/lib/view/categories/phones/nokia.dart
+++ sudaphone_sd/lib/view/categories/phones/nokia.dart
@@ -1,11 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sudaphone_sd/shared/components/custom_text.dart';
 import 'package:sudaphone_sd/shared/components/custom_title.dart';
-import 'package:sudaphone_sd/shared/components/leading.dart';
 import 'package:sudaphone_sd/shared/components/phone_list.dart';
 import 'package:sudaphone_sd/view_model/categories_view_model.dart';
+
+import '../../home/components/custom_leading.dart';
 
 class Nokia extends GetWidget<CategoriesViewModel> {
   const Nokia({Key? key}) : super(key: key);
@@ -14,9 +14,9 @@ class Nokia extends GetWidget<CategoriesViewModel> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 0,
-        title: const  CustomTitle(text: "Nokia", underLineWidget: 50),
-              leading:  const Leading(),
+        title: const  CustomTitle(text: "Nokia", underLineWidget: 50,showUnderLine: false),
+          leading:  const CustomLeading(),
+          centerTitle: true,
       ),
       body: FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
         future:
