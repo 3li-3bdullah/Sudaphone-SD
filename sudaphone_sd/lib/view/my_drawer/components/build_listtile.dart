7dcd9b3c3640104a3ef599ff6595ad5f4902c0cd
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-24 15:29:49 +0200

    Added

--- sudaphone_sd/lib/view/my_drawer/components/build_listtile.dart
+++ sudaphone_sd/lib/view/my_drawer/components/build_listtile.dart
@@ -13,16 +13,25 @@ class BuildListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      child: ListTile(
-        title: Text(
-          text,
-          style: const TextStyle(color: Colors.white, fontSize: 16),
-        ),
-        leading: Image(
-          image: AssetImage(icon),
-          color: Colors.white,
-          height: 25,
-          width: 25,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 13),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image(
+              image: AssetImage(icon),
+              color: Colors.white,
+              height: 25,
+              width: 25,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Text(
+              text,
+              style: const TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          ],
         ),
       ),
       onTap: () {
