650b611cbe58e92deb5edb4c3aaf222c45ec8dee
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-24 12:06:19 +0200

    I have set new color

--- sudaphone_sd/lib/view/no_internet_connection/no_internet_connection.dart
+++ sudaphone_sd/lib/view/no_internet_connection/no_internet_connection.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
-import 'package:sudaphone_sd/shared/components/custom_text.dart';
-import 'package:sudaphone_sd/view_model/mydrawer_view_model.dart';
-import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
 class NoInternetConnection extends StatelessWidget {
   const NoInternetConnection({Key? key}) : super(key: key);
@@ -11,53 +7,7 @@ class NoInternetConnection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Align(
-      //     alignment: Alignment.centerLeft,
-      //     child: CustomText(
-      //       text: "Sudaphone SD",
-      //       fontSize: 20,
-      //       textAlign: TextAlign.center,
-      //       fontWeight: FontWeight.normal,
-      //     ),
-      //   ),
-      //   elevation: 0,
-      //   actions: [
-      //     GetBuilder<ThemesViewModel>(
-      //       builder: (control) => Padding(
-      //         padding: const EdgeInsets.only(right: 18.0),
-      //         child: IconButton(
-      //           icon: Image(
-      //             image: const AssetImage("assets/images/icons/search.png"),
-      //             height: 22,
-      //             width: 22,
-      //             color: control.theme == ThemeMode.dark
-      //                 ? Colors.white
-      //                 : Colors.black,
-      //           ),
-      //           onPressed: () {},
-      //         ),
-      //       ),
-      //     ),
-      //   ],
-      //   leading: GetBuilder<MyDrawerViewModel>(
-      //     builder: (controller) => IconButton(
-      //       onPressed: () {
-      //         return controller.value == 0.0
-      //             ? controller.valueOne()
-      //             : controller.valueZero();
-      //       },
-      //       icon: GetBuilder<ThemesViewModel>(
-      //         builder: (control) => Icon(
-      //           controller.value == 0.0 ? Icons.menu : Icons.menu_open_outlined,
-      //           color: control.theme == ThemeMode.dark
-      //               ? Colors.white
-      //               : Colors.black,
-      //         ),
-      //       ),
-      //     ),
-      //   ),
-      // ),
+      backgroundColor: const Color(0xffe7ebec),
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
