1dc3b13770ba298a2eb48f0378d09a916aea8258
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-24 16:21:27 +0200

    I have added some features and formatted the code

--- sudaphone_sd/lib/view/categories/categories.dart
+++ sudaphone_sd/lib/view/categories/categories.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sudaphone_sd/shared/components/custom_text2.dart';
 import 'package:sudaphone_sd/shared/components/custom_title.dart';
+import 'package:sudaphone_sd/view/categories/components/categories_card.dart';
 import 'package:sudaphone_sd/view/categories/phones/huawei.dart';
 import 'package:sudaphone_sd/view/categories/phones/iphone.dart';
 import 'package:sudaphone_sd/view/categories/phones/lenovo.dart';
@@ -12,7 +12,6 @@ import 'package:sudaphone_sd/view/categories/phones/samsung.dart';
 import 'package:sudaphone_sd/view/categories/phones/tecno.dart';
 import 'package:sudaphone_sd/view/categories/phones/vivo.dart';
 import 'package:sudaphone_sd/view/categories/phones/xiaomi.dart';
-import 'package:sudaphone_sd/view/categories/components/categories_card.dart';
 import 'package:sudaphone_sd/view_model/themes_view_model.dart';
 
 class Categories extends GetWidget<ThemesViewModel> {
@@ -22,8 +21,16 @@ class Categories extends GetWidget<ThemesViewModel> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const CustomTitle(text: "Categories", underLineWidget: 70),
-        elevation: 0,
+        title: const CustomTitle(
+            text: "Categories", underLineWidget: 70, showUnderLine: false),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.grey,
+          ),
+        ),
       ),
       body: Container(
         height: double.infinity,
@@ -35,64 +42,113 @@ class Categories extends GetWidget<ThemesViewModel> {
                 crossAxisCount: 2),
             children: [
               CategoriesCard(
-                imageCat: controller.theme == ThemeMode.dark ? "assets/images/category/samsung1.jpg" :"assets/images/category/samsung.png",
+                imageCat: controller.theme == ThemeMode.dark
+                    ? "assets/images/category/samsung1.jpg"
+                    : "assets/images/category/samsung.png",
                 onTap: () {
-                  Get.to(() => const Samsung(),duration: const Duration(milliseconds: 50),
-                      transition: Transition.zoom, curve: Curves.bounceInOut);
+                  Get.to(() => const Samsung(),
+                      duration: const Duration(milliseconds: 50),
+                      transition: Transition.zoom,
+                      curve: Curves.bounceInOut);
                 },
               ),
               CategoriesCard(
-                imageCat: controller.theme == ThemeMode.dark ? "assets/images/category/huawei1.jpg" :"assets/images/category/huawei.png",
+                imageCat: controller.theme == ThemeMode.dark
+                    ? "assets/images/category/huawei1.jpg"
+                    : "assets/images/category/huawei.png",
                 onTap: () {
-                  Get.to(() => const Huawei(),duration: const Duration(milliseconds: 50),);
+                  Get.to(
+                    () => const Huawei(),
+                    duration: const Duration(milliseconds: 50),
+                  );
                 },
               ),
               CategoriesCard(
-                imageCat: controller.theme == ThemeMode.dark ? "assets/images/category/iphone1.jpg" : "assets/images/category/apple.jpg",
+                imageCat: controller.theme == ThemeMode.dark
+                    ? "assets/images/category/iphone1.jpg"
+                    : "assets/images/category/apple.jpg",
                 onTap: () {
-                  Get.to(() => const Iphone(),duration: const Duration(milliseconds: 50),);
+                  Get.to(
+                    () => const Iphone(),
+                    duration: const Duration(milliseconds: 50),
+                  );
                 },
               ),
               CategoriesCard(
-                imageCat: controller.theme == ThemeMode.dark ? "assets/images/category/oppo1.jpg" : "assets/images/category/oppo.png",
+                imageCat: controller.theme == ThemeMode.dark
+                    ? "assets/images/category/oppo1.jpg"
+                    : "assets/images/category/oppo.png",
                 onTap: () {
-                  Get.to(() => const Oppo(),duration: const Duration(milliseconds: 50),);
+                  Get.to(
+                    () => const Oppo(),
+                    duration: const Duration(milliseconds: 50),
+                  );
                 },
               ),
               CategoriesCard(
-                imageCat: controller.theme == ThemeMode.dark ? "assets/images/category/xiaomi1.jpg" : "assets/images/category/xiaomi.png",
+                imageCat: controller.theme == ThemeMode.dark
+                    ? "assets/images/category/xiaomi1.jpg"
+                    : "assets/images/category/xiaomi.png",
                 onTap: () {
-                  Get.to(() => const Xiaomi(),duration: const Duration(milliseconds: 50),);
+                  Get.to(
+                    () => const Xiaomi(),
+                    duration: const Duration(milliseconds: 50),
+                  );
                 },
               ),
               CategoriesCard(
-                imageCat: controller.theme == ThemeMode.dark ? "assets/images/category/lenovo1.jpg" : "assets/images/category/lenovo.png",
+                imageCat: controller.theme == ThemeMode.dark
+                    ? "assets/images/category/lenovo1.jpg"
+                    : "assets/images/category/lenovo.png",
                 onTap: () {
-                  Get.to(() => const Lenovo(),duration: const Duration(milliseconds: 50),);
+                  Get.to(
+                    () => const Lenovo(),
+                    duration: const Duration(milliseconds: 50),
+                  );
                 },
               ),
               CategoriesCard(
-                imageCat: controller.theme == ThemeMode.dark ? "assets/images/category/realme1.png" : "assets/images/category/realme.png",
+                imageCat: controller.theme == ThemeMode.dark
+                    ? "assets/images/category/realme1.png"
+                    : "assets/images/category/realme.png",
                 onTap: () {
-                  Get.to(() => const Realme(),duration: const Duration(milliseconds: 50),);
+                  Get.to(
+                    () => const Realme(),
+                    duration: const Duration(milliseconds: 50),
+                  );
                 },
               ),
               CategoriesCard(
-                imageCat: controller.theme == ThemeMode.dark ? "assets/images/category/tecno1.jpg" : "assets/images/category/tecno.png",
+                imageCat: controller.theme == ThemeMode.dark
+                    ? "assets/images/category/tecno1.jpg"
+                    : "assets/images/category/tecno.png",
                 onTap: () {
-                  Get.to(() => const Tecno(),duration: const Duration(milliseconds: 50),);
+                  Get.to(
+                    () => const Tecno(),
+                    duration: const Duration(milliseconds: 50),
+                  );
                 },
               ),
               CategoriesCard(
-                imageCat: controller.theme == ThemeMode.dark ? "assets/images/category/vivo1.jpg" :"assets/images/category/vivo.jpg",
+                imageCat: controller.theme == ThemeMode.dark
+                    ? "assets/images/category/vivo1.jpg"
+                    : "assets/images/category/vivo.jpg",
                 onTap: () {
-                  Get.to(() => const Vivo(),duration: const Duration(milliseconds: 50),);
+                  Get.to(
+                    () => const Vivo(),
+                    duration: const Duration(milliseconds: 50),
+                  );
                 },
               ),
               CategoriesCard(
-                imageCat: controller.theme == ThemeMode.dark ? "assets/images/category/nokia1.jpg" : "assets/images/category/nokia.png",
+                imageCat: controller.theme == ThemeMode.dark
+                    ? "assets/images/category/nokia1.jpg"
+                    : "assets/images/category/nokia.png",
                 onTap: () {
-                  Get.to(() => const Nokia(),duration: const Duration(milliseconds: 50),);
+                  Get.to(
+                    () => const Nokia(),
+                    duration: const Duration(milliseconds: 50),
+                  );
                 },
               ),
             ],
