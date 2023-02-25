9817d44ccc46cbe6304bda730856baf5f8b84fb0
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-25 18:43:58 +0200

    Added GetBuilder to switch color when mode changing

--- sudaphone_sd/lib/view/posts/components/comment_button.dart
+++ sudaphone_sd/lib/view/posts/components/comment_button.dart
@@ -2,11 +2,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sudaphone_sd/shared/components/custom_text2.dart';
+import 'package:sudaphone_sd/shared/constants.dart';
 import 'package:sudaphone_sd/view/posts/comments.dart';
 import 'package:sudaphone_sd/view_model/posts_view_model.dart';
 
+import '../../../view_model/themes_view_model.dart';
+
 class CommentButton extends GetWidget<PostsViewModel> {
-  const CommentButton({Key? key,required this.userName ,required this.currentDoc}) : super(key: key);
+  const CommentButton(
+      {Key? key, required this.userName, required this.currentDoc})
+      : super(key: key);
   final String currentDoc;
   final String userName;
 
@@ -33,12 +38,18 @@ class CommentButton extends GetWidget<PostsViewModel> {
                 .collection("comments")
                 .snapshots(),
             builder: (context, snapshot) {
-              return CustomText2(
-                text: "${snapshot.data?.docs.length}",
-                color: Colors.grey,
-                fontSize: 15,
-                fontWeight: FontWeight.normal,
-                textAlign: TextAlign.center,
+              return GetBuilder<ThemesViewModel>(
+                builder: (themeController) {
+                  return CustomText2(
+                    text: "${snapshot.data?.docs.length}",
+                    color: themeController.theme == ThemeMode.dark
+                        ? Colors.white
+                        : kBlackColor,
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                    textAlign: TextAlign.center,
+                  );
+                },
               );
             },
           ),
