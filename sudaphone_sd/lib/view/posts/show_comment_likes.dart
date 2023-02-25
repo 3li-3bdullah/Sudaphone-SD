76bcb75a2adb84e084144dfb0c3a616aef652b36
3li-3bdullah <PROGRMMER7@gmail.com>
2023-02-25 18:55:11 +0200

    Added the ScreenLeading

--- sudaphone_sd/lib/view/posts/show_comment_likes.dart
+++ sudaphone_sd/lib/view/posts/show_comment_likes.dart
@@ -1,14 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sudaphone_sd/shared/components/custom_leading.dart';
 import 'package:sudaphone_sd/shared/components/custom_text.dart';
 import 'package:sudaphone_sd/shared/components/custom_title.dart';
-import 'package:sudaphone_sd/shared/components/leading.dart';
 import 'package:sudaphone_sd/view_model/posts_view_model.dart';
 
-
 class ShowCommentLikes extends GetWidget<PostsViewModel> {
-  const ShowCommentLikes({required this.peopleWhoLiked,required this.currentDoc , Key? key}) : super(key: key);
+  const ShowCommentLikes(
+      {required this.peopleWhoLiked, required this.currentDoc, Key? key})
+      : super(key: key);
   final peopleWhoLiked;
   final currentDoc;
   @override
@@ -17,46 +18,45 @@ class ShowCommentLikes extends GetWidget<PostsViewModel> {
       appBar: AppBar(
         title: const CustomTitle(text: "People who liked", underLineWidget: 80),
         elevation: 0,
-        leading: const Leading(),
+        leading: const ScreensLeading(),
       ),
       body: ListView.builder(
         itemCount: peopleWhoLiked.length,
         itemBuilder: (context, index) {
           return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-            future: controller.userInfoCollection
-                .doc(peopleWhoLiked[index])
-                .get(),
+            future:
+                controller.userInfoCollection.doc(peopleWhoLiked[index]).get(),
             builder: ((context, snapshot) {
-             if(snapshot.hasData){
-               if(currentDoc['${peopleWhoLiked[index]}']){
-                return Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: ListTile(
-                  title: Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: CustomText(
-                        text: "${snapshot.data!.data()!['userName']}",
-                        fontSize: 17,
-                        fontWeight: FontWeight.normal,
-                        textAlign: TextAlign.left),
-                  ),
-                  leading : CircleAvatar(
-                    radius: 30,
-                    backgroundImage:
-                        NetworkImage(snapshot.data?.data()!['profileUrl']),
-                  ),
-                ),
-              );
+              if (snapshot.hasData) {
+                if (currentDoc['${peopleWhoLiked[index]}']) {
+                  return Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: ListTile(
+                      title: Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: CustomText(
+                            text: "${snapshot.data!.data()!['userName']}",
+                            fontSize: 17,
+                            fontWeight: FontWeight.normal,
+                            textAlign: TextAlign.left),
+                      ),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage:
+                            NetworkImage(snapshot.data?.data()!['profileUrl']),
+                      ),
+                    ),
+                  );
+                } else {
+                  return const SizedBox();
+                }
               } else {
-                return const SizedBox();
+                return Container(
+                    alignment: Alignment.center,
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.asset('assets/images/loader.gif'));
               }
-             }else{
-              return Container(
-                alignment: Alignment.center,
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                child: Image.asset('assets/images/loader.gif'));
-             }
             }),
           );
         },
